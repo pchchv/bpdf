ae9be63f5906e5f5b4937d075fa982c48ed4c895
pchchv <ipchchv@gmail.com>
2024-10-19 20:54:43 +0300

    Implement Provider type

---
+++ consts/provider/provider.go
@@ -0,0 +1,10 @@
+// The provider package contains all document generation providers.
+package provider
+
+// Provider is a representation of a document generation provider.
+type Provider string
+
+const (
+	// Gofpdf represents the gofpdf provider.
+	Gofpdf Provider = "gofpdf"
+)
