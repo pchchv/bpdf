66c3ba73ef79314101ba6583c88919d26e3062b6
pchchv <ipchchv@gmail.com>
2024-10-19 21:11:35 +0300

    Implement a Fontstyle type

---
+++ consts/fontstyle/fontstyle.go
@@ -0,0 +1,17 @@
+// The fontstyle package contains all default font styles.
+package fontstyle
+
+// Type is a representation of a style DefaultFont.
+type Fontstyle string
+
+const (
+	Bold       Fontstyle = "B"
+	Normal     Fontstyle = ""
+	Italic     Fontstyle = "I"
+	BoldItalic Fontstyle = "BI"
+)
+
+// IsValid checks if the style is valid.
+func (s Fontstyle) IsValid() bool {
+	return s == Normal || s == Italic || s == BoldItalic || s == Bold
+}
