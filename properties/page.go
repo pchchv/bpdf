53ce3834f54cafdf62f0a881a2272a24056a68bb
pchchv <ipchchv@gmail.com>
2024-10-19 21:09:04 +0300

    Implement an IsValid method of the Place type

--- properties/page.go
+++ properties/page.go
@@ -1,4 +1,19 @@
 package properties
 
+const (
+	Top         Place = "top"          // place in the top of the page
+	LeftTop     Place = "left_top"     // place in the left top of the page
+	RightTop    Place = "right_top"    // place in the right top of the page
+	Bottom      Place = "bottom"       // place in the bottom of the page
+	LeftBottom  Place = "left_bottom"  // place in the left bottom of the page
+	RightBottom Place = "right_bottom" // place in the right bottom of the page
+)
+
 // Place is the representation of a place in a page.
 type Place string
+
+// IsValid checks if the place is valid.
+func (p Place) IsValid() bool {
+	return p == LeftTop || p == Top || p == RightTop ||
+		p == LeftBottom || p == Bottom || p == RightBottom
+}
