c1d7980b3a4e9d18c799150c835f67c8d023d0a0
pchchv <ipchchv@gmail.com>
2024-10-19 21:17:56 +0300

    Implement a PageNumber type

--- properties/page.go
+++ properties/page.go
@@ -1,5 +1,7 @@
 package properties
 
+import "github.com/pchchv/bpdf/consts/fontstyle"
+
 const (
 	Top         Place = "top"          // place in the top of the page
 	LeftTop     Place = "left_top"     // place in the left top of the page
@@ -16,4 +18,20 @@ type Place string
 func (p Place) IsValid() bool {
 	return p == LeftTop || p == Top || p == RightTop ||
 		p == LeftBottom || p == Bottom || p == RightBottom
+}
+
+// PageNumber have attributes of page number
+type PageNumber struct {
+	// Pattern is the string pattern which will be used to apply the page count component.
+	Pattern string
+	// Place defines where the page count component will be placed.
+	Place Place
+	// Family defines which font family will be applied to page count.
+	Family string
+	// Style defines which font style will be applied to page count.
+	Style fontstyle.Fontstyle
+	// Size defines which font size will be applied to page count.
+	Size float64
+	// Color defines which will be applied to page count.
+	Color *Color
 }
