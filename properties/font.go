6613646ef05b248f1c9b4f7db98429a25d19f000
pchchv <ipchchv@gmail.com>
2024-10-20 19:10:52 +0300

    Implement an AppendMap method

--- properties/font.go
+++ properties/font.go
@@ -9,3 +9,24 @@ type Font struct {
 	Size   float64
 	Color  *Color
 }
+
+// AppendMap appends the font fields to a map.
+func (f *Font) AppendMap(m map[string]interface{}) map[string]interface{} {
+	if f.Family != "" {
+		m["prop_font_family"] = f.Family
+	}
+
+	if f.Style != "" {
+		m["prop_font_style"] = f.Style
+	}
+
+	if f.Size != 0 {
+		m["prop_font_size"] = f.Size
+	}
+
+	if f.Color != nil {
+		m["prop_font_color"] = f.Color.ToString()
+	}
+
+	return m
+}
