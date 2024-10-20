5b46ce6c781d845f7a4235c13d2c4fe543b3bd66
pchchv <ipchchv@gmail.com>
2024-10-20 19:20:42 +0300

    Create a ToString method

--- core/entity/metadata.go
+++ core/entity/metadata.go
@@ -1,7 +1,14 @@
 package entity
 
+import "fmt"
+
 // Utf8Text representats a text with a flag to indicate if it's UTF8.
 type Utf8Text struct {
 	Text string
 	UTF8 bool
+}
+
+// ToString returns a string representation of the text.
+func (u *Utf8Text) ToString() string {
+	return fmt.Sprintf("Utf8Text(%s, %v)", u.Text, u.UTF8)
 }
