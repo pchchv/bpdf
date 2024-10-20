527e892d20ea9bebb306a330dddd6ee1cb3b9104
pchchv <ipchchv@gmail.com>
2024-10-20 19:21:09 +0300

    Create a type Metadata

--- core/entity/metadata.go
+++ core/entity/metadata.go
@@ -1,6 +1,9 @@
 package entity
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Utf8Text representats a text with a flag to indicate if it's UTF8.
 type Utf8Text struct {
@@ -11,4 +14,14 @@ type Utf8Text struct {
 // ToString returns a string representation of the text.
 func (u *Utf8Text) ToString() string {
 	return fmt.Sprintf("Utf8Text(%s, %v)", u.Text, u.UTF8)
+}
+
+// Metadata is the representation of a PDF metadata.
+type Metadata struct {
+	Author       *Utf8Text
+	Creator      *Utf8Text
+	Subject      *Utf8Text
+	Title        *Utf8Text
+	CreationDate *time.Time
+	KeywordsStr  *Utf8Text
 }
