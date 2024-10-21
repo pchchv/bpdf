d9e4eeb30ced0d6e8ae0f8c327e235217c135943
pchchv <ipchchv@gmail.com>
2024-10-21 13:35:17 +0300

    Create test of an AppendMap method

--- core/entity/metadata_test.go
+++ core/entity/metadata_test.go
@@ -1,6 +1,24 @@
 package entity
 
-import "time"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetadata_AppendMap(t *testing.T) {
+	sut := fixtureMetadata()
+	m := make(map[string]interface{})
+	sut.AppendMap(m)
+
+	assert.Equal(t, "Utf8Text(author, true)", m["config_metadata_author"])
+	assert.Equal(t, "Utf8Text(creator, false)", m["config_metadata_creator"])
+	assert.Equal(t, "Utf8Text(subject, true)", m["config_metadata_subject"])
+	assert.Equal(t, "Utf8Text(title, true)", m["config_metadata_title"])
+	assert.Equal(t, "Utf8Text(keyword, true)", m["config_metadata_keywords"])
+	assert.Equal(t, true, m["config_metadata_creation_date"])
+}
 
 func fixtureMetadata() Metadata {
 	now := time.Now()
