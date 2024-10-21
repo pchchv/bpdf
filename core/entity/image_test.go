6228f5ff37840f327fa6b65678d9c4660f813392
pchchv <ipchchv@gmail.com>
2024-10-21 13:32:53 +0300

    Create test of an AppendMap method

--- core/entity/image_test.go
+++ core/entity/image_test.go
@@ -1,6 +1,22 @@
 package entity
 
-import "github.com/pchchv/bpdf/consts/extension"
+import (
+	"testing"
+
+	"github.com/pchchv/bpdf/consts/extension"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestImage_AppendMap(t *testing.T) {
+	sut := fixtureImage()
+	m := make(map[string]interface{})
+	m = sut.AppendMap(m)
+
+	assert.Equal(t, "[1 2 3]", m["entity_image_bytes"])
+	assert.Equal(t, extension.Png, m["entity_extension"])
+	assert.Equal(t, 100.0, m["background_dimension_width"])
+	assert.Equal(t, 200.0, m["background_dimension_height"])
+}
 
 func fixtureImage() Image {
 	dimensions := fixtureDimensions()
