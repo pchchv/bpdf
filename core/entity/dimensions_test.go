8263d524b4af25902cafd02d7f4b594eb8888253
pchchv <ipchchv@gmail.com>
2024-10-21 13:30:57 +0300

    Create test of an AppendMap method

--- core/entity/dimensions_test.go
+++ core/entity/dimensions_test.go
@@ -1,5 +1,20 @@
 package entity
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDimensions_AppendMap(t *testing.T) {
+	sut := fixtureDimensions()
+	m := make(map[string]interface{})
+	m = sut.AppendMap("label", m)
+
+	assert.Equal(t, 100.0, m["label_dimension_width"])
+	assert.Equal(t, 200.0, m["label_dimension_height"])
+}
+
 func fixtureDimensions() Dimensions {
 	return Dimensions{
 		Width:  100,
