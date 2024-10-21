03b10f9e90ced8d663632671f0bebb14e03bfdaf
pchchv <ipchchv@gmail.com>
2024-10-21 13:45:04 +0300

    Create fontfamily package

---
+++ consts/fontfamily/fontfamily.go
@@ -0,0 +1,10 @@
+// The fontfamily package contains all default font families.
+package fontfamily
+
+const (
+	Arial     string = "arial"        // represents an arial DefaultFont
+	Helvetica string = "helvetica"    // represents a helvetica DefaultFont
+	Symbol    string = "symbol"       // represents a symbol DefaultFont
+	ZapBats   string = "zapfdingbats" // represents a zapfdingbats DefaultFont
+	Courier   string = "courier"      // represents a courier DefaultFont
+)
