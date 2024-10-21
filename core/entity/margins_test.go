d825d6c50af5c8d8b90ef4da49d764c906dce206
pchchv <ipchchv@gmail.com>
2024-10-21 13:34:03 +0300

    Create test of an AppendMap method

--- core/entity/margins_test.go
+++ core/entity/margins_test.go
@@ -1,5 +1,22 @@
 package entity
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMargins_AppendMap(t *testing.T) {
+	sut := fixtureMargins()
+	m := make(map[string]interface{})
+	m = sut.AppendMap(m)
+
+	assert.Equal(t, 20.0, m["config_margin_left"])
+	assert.Equal(t, 30.0, m["config_margin_top"])
+	assert.Equal(t, 40.0, m["config_margin_right"])
+	assert.Equal(t, 50.0, m["config_margin_bottom"])
+}
+
 func fixtureMargins() Margins {
 	return Margins{
 		Left:   20,
