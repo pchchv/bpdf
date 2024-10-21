fb66a477c2868364b4907bf109ffd47991e36c2a
pchchv <ipchchv@gmail.com>
2024-10-21 13:53:07 +0300

    Create test of MakeValid method

--- properties/rect_test.go
+++ properties/rect_test.go
@@ -4,8 +4,47 @@ import (
 	"testing"
 
 	"github.com/pchchv/bpdf/internal/fixture"
+	"github.com/pchchv/bpdf/properties"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestRect_MakeValid(t *testing.T) {
+	t.Run("when percent is less than zero, should become 100", func(t *testing.T) {
+		prop := properties.Rect{Percent: -2}
+		prop.MakeValid()
+
+		assert.Equal(t, prop.Percent, 100.0)
+	})
+
+	t.Run("when percent is greater than 100, should become 100", func(t *testing.T) {
+		prop := properties.Rect{Percent: 102}
+		prop.MakeValid()
+
+		assert.Equal(t, prop.Percent, 100.0)
+	})
+
+	t.Run("when is center, top and left should become 0", func(t *testing.T) {
+		prop := properties.Rect{Center: true, Top: 5, Left: 5}
+		prop.MakeValid()
+
+		assert.Equal(t, prop.Top, 0.0)
+		assert.Equal(t, prop.Left, 0.0)
+	})
+
+	t.Run("when left is less than 0, should become 0", func(t *testing.T) {
+		prop := properties.Rect{Left: -5}
+		prop.MakeValid()
+
+		assert.Equal(t, prop.Left, 0.0)
+	})
+
+	t.Run("when top is less than 0, should become 0", func(t *testing.T) {
+		prop := properties.Rect{Top: -5}
+		prop.MakeValid()
+
+		assert.Equal(t, prop.Top, 0.0)
+	})
+}
 
 func TestRect_ToMap(t *testing.T) {
 	sut := fixture.RectProp()
