75a6874ab94ecabf53549a06a2a718f4d58e47a0
pchchv <ipchchv@gmail.com>
2024-10-21 11:48:39 +0300

    Implement an AppendMap method

--- core/entity/image.go
+++ core/entity/image.go
@@ -1,10 +1,38 @@
 package entity
 
-import "github.com/pchchv/bpdf/consts/extension"
+import (
+	"fmt"
+
+	"github.com/pchchv/bpdf/consts/extension"
+)
+
+const trimSize = 10
 
 // Image is the representation of an image that can be added to the pdf.
 type Image struct {
 	Bytes      []byte
 	Extension  extension.Extension
 	Dimensions *Dimensions
+}
+
+// AppendMap adds the Image fields to the map.
+func (i *Image) AppendMap(m map[string]interface{}) map[string]interface{} {
+	lenBytes := len(i.Bytes)
+	if lenBytes != 0 {
+		trimSize := trimSize
+		if lenBytes < trimSize {
+			trimSize = lenBytes
+		}
+		m["entity_image_bytes"] = fmt.Sprintf("%v", i.Bytes[:trimSize])
+	}
+
+	if i.Extension != "" {
+		m["entity_extension"] = i.Extension
+	}
+
+	if i.Dimensions != nil {
+		m = i.Dimensions.AppendMap("background", m)
+	}
+
+	return m
 }
