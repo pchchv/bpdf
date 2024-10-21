4f64035b84368498b4a23cc2217710abc800a4e8
pchchv <ipchchv@gmail.com>
2024-10-21 13:36:32 +0300

    Create test of an AppendMap method

--- core/entity/protection_test.go
+++ core/entity/protection_test.go
@@ -1,6 +1,21 @@
 package entity
 
-import "github.com/pchchv/bpdf/consts/protection"
+import (
+	"testing"
+
+	"github.com/pchchv/bpdf/consts/protection"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProtection_AppendMap(t *testing.T) {
+	sut := fixtureProtection()
+	m := make(map[string]interface{})
+	m = sut.AppendMap(m)
+
+	assert.Equal(t, sut.Type, m["config_protection_type"])
+	assert.Equal(t, sut.UserPassword, m["config_user_password"])
+	assert.Equal(t, sut.OwnerPassword, m["config_owner_password"])
+}
 
 func fixtureProtection() Protection {
 	return Protection{
