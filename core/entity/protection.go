f77f35dbd537553c4b15b6db4a9d110df7298260
pchchv <ipchchv@gmail.com>
2024-10-21 11:57:18 +0300

    Implement an AppendMap method

--- core/entity/protection.go
+++ core/entity/protection.go
@@ -8,3 +8,20 @@ type Protection struct {
 	UserPassword  string
 	OwnerPassword string
 }
+
+// AppendMap adds the Protection fields to the map.
+func (p *Protection) AppendMap(m map[string]interface{}) map[string]interface{} {
+	if p.Type != 0 {
+		m["config_protection_type"] = p.Type
+	}
+
+	if p.UserPassword != "" {
+		m["config_user_password"] = p.UserPassword
+	}
+
+	if p.OwnerPassword != "" {
+		m["config_owner_password"] = p.OwnerPassword
+	}
+
+	return m
+}
