11f7cc418adf2661714f72165debe27497d3a79f
pchchv <ipchchv@gmail.com>
2024-10-21 13:55:12 +0300

    Implement RectProp and TextProp funcs

--- internal/fixture/fixture.go
+++ internal/fixture/fixture.go
@@ -1,6 +1,8 @@
 package fixture
 
 import (
+	"github.com/pchchv/bpdf/consts/align"
+	"github.com/pchchv/bpdf/consts/breakline"
 	"github.com/pchchv/bpdf/consts/fontfamily"
 	"github.com/pchchv/bpdf/consts/fontstyle"
 	"github.com/pchchv/bpdf/properties"
@@ -25,5 +27,38 @@ func FontProp() properties.Font {
 		Color:  &colorProp,
 	}
 	prop.MakeValid(fontfamily.Arial)
+	return prop
+}
+
+// RectProp is responsible to give a valid properties.Rect.
+func RectProp() properties.Rect {
+	prop := properties.Rect{
+		Top:     10,
+		Left:    10,
+		Percent: 98,
+		Center:  false,
+	}
+	prop.MakeValid()
+	return prop
+}
+
+// TextProp is responsible to give a valid properties.Text.
+func TextProp() properties.Text {
+	fontProp := FontProp()
+	google := "https://www.google.com"
+	prop := properties.Text{
+		Top:               12,
+		Bottom:            13,
+		Left:              3,
+		Family:            fontProp.Family,
+		Style:             fontProp.Style,
+		Size:              fontProp.Size,
+		Align:             align.Right,
+		BreakLineStrategy: breakline.DashStrategy,
+		VerticalPadding:   20,
+		Color:             fontProp.Color,
+		Hyperlink:         &google,
+	}
+	prop.MakeValid(&fontProp)
 	return prop
 }
