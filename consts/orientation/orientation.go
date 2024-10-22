fba5d8f07fbbbb53cba3eb819266def3141c38bd
pchchv <ipchchv@gmail.com>
2024-10-22 11:31:10 +0300

    Implement an Orientation type

---
+++ consts/orientation/orientation.go
@@ -0,0 +1,10 @@
+// Package orientation contains all orientations.
+package orientation
+
+// Type is a representation of a page orientation.
+type Orient string
+
+const (
+	Vertical   Orient = "vertical"   // portrait
+	Horizontal Orient = "horizontal" // landscape
+)
