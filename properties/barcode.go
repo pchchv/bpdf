3c56267b74c62d3039e04915628aecd9352d7005
pchchv <ipchchv@gmail.com>
2024-10-22 12:26:35 +0300

    Implement a ToMap and a ToRectProp methods

--- properties/barcode.go
+++ properties/barcode.go
@@ -20,3 +20,47 @@ type Barcode struct {
 	// Type represents the barcode type. Default: code128
 	Type barcode.Code
 }
+
+// ToMap from Barcode will return a map representation from Barcode.
+func (b *Barcode) ToMap() map[string]interface{} {
+	if b == nil {
+		return nil
+	}
+
+	m := make(map[string]interface{})
+	if b.Left != 0 {
+		m["prop_left"] = b.Left
+	}
+
+	if b.Top != 0 {
+		m["prop_top"] = b.Top
+	}
+
+	if b.Percent != 0 {
+		m["prop_percent"] = b.Percent
+	}
+
+	if b.Proportion.Width != 0 {
+		m["prop_proportion_width"] = b.Proportion.Width
+	}
+
+	if b.Proportion.Height != 0 {
+		m["prop_proportion_height"] = b.Proportion.Height
+	}
+
+	if b.Center {
+		m["prop_center"] = b.Center
+	}
+
+	return m
+}
+
+// ToRectProp from Barcode will return a Rect representation from Barcode.
+func (b *Barcode) ToRectProp() *Rect {
+	return &Rect{
+		Left:    b.Left,
+		Top:     b.Top,
+		Percent: b.Percent,
+		Center:  b.Center,
+	}
+}
