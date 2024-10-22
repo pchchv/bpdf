fc79a935ee22c7fdf268c4c39b9000a9e68904d9
pchchv <ipchchv@gmail.com>
2024-10-22 12:25:51 +0300

    Create a Barcode type

---
+++ properties/barcode.go
@@ -0,0 +1,22 @@
+package properties
+
+import "github.com/pchchv/bpdf/consts/barcode"
+
+// Barcode represents properties from a barcode inside a cell.
+type Barcode struct {
+	// Left is the space between the left cell boundary to the barcode, if center is false.
+	Left float64
+	// Top is space between the upper cell limit to the barcode, if center is false.
+	Top float64
+	// Percent is how much the barcode will occupy the cell,
+	// ex 100%: The barcode will fulfill the entire cell
+	// ex 50%: The greater side from the barcode will have half the size of the cell.
+	Percent float64
+	// Proportion is the proportion between size of the barcode.
+	// Ex: 16x9, 4x3...
+	Proportion Proportion
+	// Center define that the barcode will be vertically and horizontally centralized.
+	Center bool
+	// Type represents the barcode type. Default: code128
+	Type barcode.Code
+}
