1baaeee75637267d57186823039dc8806c010e8a
pchchv <ipchchv@gmail.com>
2024-10-22 11:40:14 +0300

    Create test of method MakeValid

---
+++ properties/line_test.go
@@ -0,0 +1,75 @@
+package properties_test
+
+import (
+	"testing"
+
+	"github.com/pchchv/bpdf/consts/linestyle"
+	"github.com/pchchv/bpdf/consts/orientation"
+	"github.com/pchchv/bpdf/properties"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLine_MakeValid(t *testing.T) {
+	t.Run("when style is empty, should apply solid", func(t *testing.T) {
+		prop := properties.Line{
+			Style: "",
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, linestyle.Solid, prop.Style)
+	})
+
+	t.Run("when thickness is 0.0, should apply default", func(t *testing.T) {
+		prop := properties.Line{
+			Thickness: 0.0,
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, 0.2, prop.Thickness)
+	})
+
+	t.Run("when orientation is empty, should apply horizontal", func(t *testing.T) {
+		prop := properties.Line{
+			Orientation: "",
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, orientation.Horizontal, prop.Orientation)
+	})
+
+	t.Run("when offset percent is less than 5, should apply 5", func(t *testing.T) {
+		prop := properties.Line{
+			OffsetPercent: 4,
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, 5.0, prop.OffsetPercent)
+	})
+
+	t.Run("when offset percent is greater than 95, should apply 95", func(t *testing.T) {
+		prop := properties.Line{
+			OffsetPercent: 96,
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, 95.0, prop.OffsetPercent)
+	})
+
+	t.Run("when size percent is less than 1, should apply 90", func(t *testing.T) {
+		prop := properties.Line{
+			SizePercent: 0,
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, 90.0, prop.SizePercent)
+	})
+
+	t.Run("when size percent is greater than 100, should apply 100", func(t *testing.T) {
+		prop := properties.Line{
+			SizePercent: 101,
+		}
+		prop.MakeValid()
+
+		assert.Equal(t, 100.0, prop.SizePercent)
+	})
+}
