939ed117ddbd3cd014c724e34e1171b279a64fd2
pchchv <ipchchv@gmail.com>
2024-10-24 10:00:22 +0300

    Create tests of Copy and GetDimensions methods

--- core/entity/cell_test.go
+++ core/entity/cell_test.go
@@ -7,6 +7,55 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCell_GetDimensions(t *testing.T) {
+	cell := entity.Cell{
+		X:      10,
+		Y:      10,
+		Width:  100,
+		Height: 100,
+	}
+	dimensions := cell.GetDimensions()
+
+	assert.Equal(t, 100.0, dimensions.Width)
+	assert.Equal(t, 100.0, dimensions.Height)
+}
+
+func TestCell_Copy(t *testing.T) {
+	t.Run("copy should return same values", func(t *testing.T) {
+		cell := entity.Cell{
+			X:      10,
+			Y:      10,
+			Width:  100,
+			Height: 100,
+		}
+		copyCell := cell.Copy()
+
+		assert.Equal(t, cell.X, copyCell.X)
+		assert.Equal(t, cell.Y, copyCell.Y)
+		assert.Equal(t, cell.Width, copyCell.Width)
+		assert.Equal(t, cell.Height, copyCell.Height)
+	})
+
+	t.Run("copy should not allow side-effects", func(t *testing.T) {
+		cell := entity.Cell{
+			X:      10,
+			Y:      10,
+			Width:  100,
+			Height: 100,
+		}
+		copyCell := cell.Copy()
+		copyCell.X = 15
+		copyCell.Y = 15
+		copyCell.Width = 90
+		copyCell.Height = 90
+
+		assert.Equal(t, 10.0, cell.X)
+		assert.Equal(t, 10.0, cell.Y)
+		assert.Equal(t, 100.0, cell.Width)
+		assert.Equal(t, 100.0, cell.Height)
+	})
+}
+
 func TestNewRootContext(t *testing.T) {
 	width := 100.0
 	height := 300.0
