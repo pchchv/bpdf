395461f3dc7df8213ee61f328aa7760993fe0635
pchchv <ipchchv@gmail.com>
2024-10-24 10:33:46 +0300

    Implement a ToMap method

--- core/entity/config.go
+++ core/entity/config.go
@@ -24,3 +24,59 @@ type Config struct {
 	BackgroundImage      *Image
 	DisableAutoPageBreak bool
 }
+
+// ToMap converts Config to a map[string]interface{} .
+func (c *Config) ToMap() map[string]interface{} {
+	m := make(map[string]interface{})
+	if c.ProviderType != "" {
+		m["config_provider_type"] = c.ProviderType
+	}
+
+	if c.Dimensions != nil {
+		m = c.Dimensions.AppendMap("bpdf", m)
+	}
+
+	if c.Margins != nil {
+		m = c.Margins.AppendMap(m)
+	}
+
+	if c.DefaultFont != nil {
+		m = c.DefaultFont.AppendMap(m)
+	}
+
+	m["generation_mode"] = c.GenerationMode
+	m["chunk_workers"] = c.ChunkWorkers
+	if c.Debug {
+		m["config_debug"] = c.Debug
+	}
+
+	if c.MaxGridSize != 0 {
+		m["config_max_grid_sum"] = c.MaxGridSize
+	}
+
+	if c.PageNumber != nil {
+		m = c.PageNumber.AppendMap(m)
+	}
+
+	if c.Protection != nil {
+		m = c.Protection.AppendMap(m)
+	}
+
+	if c.Compression {
+		m["config_compression"] = c.Compression
+	}
+
+	if c.Metadata != nil {
+		m = c.Metadata.AppendMap(m)
+	}
+
+	if c.BackgroundImage != nil {
+		m = c.BackgroundImage.AppendMap(m)
+	}
+
+	if c.DisableAutoPageBreak {
+		m["config_disable_auto_page_break"] = c.DisableAutoPageBreak
+	}
+
+	return m
+}
