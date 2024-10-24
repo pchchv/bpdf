c82250ca44033472f07ca49d6f9f231933fb1dd0
pchchv <ipchchv@gmail.com>
2024-10-24 10:48:01 +0300

    Implement Normalize and String methods

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,6 +1,14 @@
 // Package metrics contains metrics models, constants and formatting.
 package metrics
 
+import "fmt"
+
+const (
+	Nano  TimeScale = "ns" // time scale in nanoseconds
+	Micro TimeScale = "μs" // time scale in microseconds
+	Milli TimeScale = "ms" // time scale in milliseconds
+)
+
 type (
 	TimeScale string
 	SizeScale string
@@ -10,4 +18,26 @@ type (
 type Time struct {
 	Value float64
 	Scale TimeScale
+}
+
+// Normalize normalizes the time scale.
+func (t *Time) Normalize() bool {
+	if t.Scale == Nano {
+		t.Scale = Micro
+		t.Value /= 1000.0
+		return true
+	}
+
+	if t.Scale == Micro {
+		t.Scale = Milli
+		t.Value /= 1000.0
+		return true
+	}
+
+	return false
+}
+
+// String returns the time formatted.
+func (t *Time) String() string {
+	return fmt.Sprintf("%.2f%s", t.Value, t.Scale)
 }
