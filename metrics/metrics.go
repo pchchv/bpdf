a7e2457793e7a951f3e88f3cfabd88a45cd1a1cf
pchchv <ipchchv@gmail.com>
2024-10-24 10:50:47 +0300

    Implement Normalize and String methods

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -4,9 +4,13 @@ package metrics
 import "fmt"
 
 const (
-	Nano  TimeScale = "ns" // time scale in nanoseconds
-	Micro TimeScale = "μs" // time scale in microseconds
-	Milli TimeScale = "ms" // time scale in milliseconds
+	Nano     TimeScale = "ns" // time scale in nanoseconds
+	Micro    TimeScale = "μs" // time scale in microseconds
+	Milli    TimeScale = "ms" // time scale in milliseconds
+	Byte     SizeScale = "b"  // size scale in bytes
+	KiloByte SizeScale = "Kb" // size scale in kilobytes
+	MegaByte SizeScale = "Mb" // size scale in megabytes
+	GigaByte SizeScale = "Gb" // size scale in gigabytes
 )
 
 type (
@@ -46,4 +50,32 @@ func (t *Time) String() string {
 type Size struct {
 	Value float64
 	Scale SizeScale
+}
+
+// Normalize normalizes the size scale.
+func (t *Size) Normalize() bool {
+	if t.Scale == Byte {
+		t.Scale = KiloByte
+		t.Value /= 1000.0
+		return true
+	}
+
+	if t.Scale == KiloByte {
+		t.Scale = MegaByte
+		t.Value /= 1000.0
+		return true
+	}
+
+	if t.Scale == MegaByte {
+		t.Scale = GigaByte
+		t.Value /= 1000.0
+		return true
+	}
+
+	return false
+}
+
+// String returns the size formatted.
+func (t *Size) String() string {
+	return fmt.Sprintf("%.2f%s", t.Value, t.Scale)
 }
