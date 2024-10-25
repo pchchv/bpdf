f4538c3adbb6595363f851996be1cd00f3902da9
pchchv <ipchchv@gmail.com>
2024-10-25 14:26:12 +0300

    Implement a Code type

--- core/components.go
+++ core/components.go
@@ -1,9 +1,19 @@
 package core
 
-import "github.com/pchchv/bpdf/core/entity"
+import (
+	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/properties"
+)
 
 // Math is the abstraction which deals with useful calc.
 type Math interface {
 	GetInnerCenterCell(inner *entity.Dimensions, outer *entity.Dimensions) *entity.Cell
 	Resize(inner *entity.Dimensions, outer *entity.Dimensions, percent float64, justReferenceWidth bool) *entity.Dimensions
+}
+
+// Code is the abstraction which deals of how to add QrCodes or Barcode in a PDF.
+type Code interface {
+	GenQr(code string) (*entity.Image, error)
+	GenDataMatrix(code string) (*entity.Image, error)
+	GenBar(code string, cell *entity.Cell, prop *properties.Barcode) (*entity.Image, error)
 }
