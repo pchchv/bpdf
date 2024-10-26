3dc9af44706b9493cc49e03570c4210fbfb1f506
pchchv <ipchchv@gmail.com>
2024-10-26 13:27:17 +0300

    Implement AddImage and GetImage methods

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"errors"
 	"os"
 
 	"github.com/pchchv/bpdf/consts/extension"
@@ -22,4 +23,19 @@ func (c *cache) LoadImage(file string, extension extension.Extension) error {
 	img := &entity.Image{Bytes: imageBytes, Extension: extension}
 	c.images[file+string(extension)] = img
 	return nil
+}
+
+// AddImage adds an image to the cache.
+func (c *cache) AddImage(value string, image *entity.Image) {
+	c.images[value+string(image.Extension)] = image
+}
+
+// GetImage returns an image from the cache.
+func (c *cache) GetImage(file string, extension extension.Extension) (*entity.Image, error) {
+	image, ok := c.images[file+string(extension)]
+	if ok {
+		return image, nil
+	}
+
+	return nil, errors.New("image not found")
 }
