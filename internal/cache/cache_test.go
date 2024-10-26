8c6cde8dd1332a7280c2843c85a842a882438b00
pchchv <ipchchv@gmail.com>
2024-10-26 13:32:43 +0300

    Create test of an AddImage and GetImage methods

--- internal/cache/cache_test.go
+++ internal/cache/cache_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/pchchv/bpdf/consts/extension"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/cache"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,4 +15,37 @@ func TestNew(t *testing.T) {
 
 	assert.NotNil(t, sut)
 	assert.Equal(t, "*cache.cache", fmt.Sprintf("%T", sut))
+}
+
+func TestCache_GetImage(t *testing.T) {
+	t.Run("when cannot get image, should return error", func(t *testing.T) {
+		sut := cache.New()
+		img, err := sut.GetImage("image", extension.Jpg)
+
+		assert.Nil(t, img)
+		assert.NotNil(t, err)
+	})
+	t.Run("when can get image, should return image", func(t *testing.T) {
+		sut := cache.New()
+		sut.AddImage("image", &entity.Image{
+			Extension: extension.Jpg,
+		})
+		img, err := sut.GetImage("image", extension.Jpg)
+
+		assert.NotNil(t, img)
+		assert.Nil(t, err)
+	})
+}
+
+func TestCache_AddImage(t *testing.T) {
+	t.Run("when add image, return works", func(t *testing.T) {
+		sut := cache.New()
+		sut.AddImage("image", &entity.Image{
+			Extension: extension.Jpg,
+		})
+
+		img, err := sut.GetImage("image", extension.Jpg)
+		assert.NotNil(t, img)
+		assert.Nil(t, err)
+	})
 }
