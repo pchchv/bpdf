7caf94183ea2546479295566229473263b211cb3
pchchv <ipchchv@gmail.com>
2024-10-26 13:25:02 +0300

    Implement a method LoadImage

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -6,3 +6,15 @@ type cache struct {
 	images map[string]*entity.Image
 	codes  map[string][]byte
 }
+
+// LoadImage loads an image from a file.
+func (c *cache) LoadImage(file string, extension extension.Extension) error {
+	imageBytes, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	img := &entity.Image{Bytes: imageBytes, Extension: extension}
+	c.images[file+string(extension)] = img
+	return nil
+}
