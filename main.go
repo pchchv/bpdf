ba1086a051e7b3e9112d2645c55cd763cff70cc6
pchchv <ipchchv@gmail.com>
2024-10-26 13:47:42 +0300

    Create bpdf type

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package bpdf
+
+import (
+	"github.com/f-amaral/go-async/async"
+	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/internal/cache"
+)
+
+type bpdf struct {
+	config   *entity.Config
+	provider core.Provider
+	cache    cache.Cache
+	// Building
+	cell          entity.Cell
+	pages         []core.Page
+	rows          []core.Row
+	header        []core.Row
+	footer        []core.Row
+	headerHeight  float64
+	footerHeight  float64
+	currentHeight float64
+	// Processing
+	pool async.Processor[[]core.Page, []byte]
+}
