ed17ff6c390741de5519540bf32a3a3ff97ddece
pchchv <ipchchv@gmail.com>
2024-10-26 13:11:33 +0300

    Create a tests of GetBase64 and GetBytes methods

--- core/pdf_test.go
+++ core/pdf_test.go
@@ -14,3 +14,17 @@ func TestNewPDF(t *testing.T) {
 	assert.NotNil(t, sut)
 	assert.Equal(t, "*core.Pdf", fmt.Sprintf("%T", sut))
 }
+
+func TestPdf_GetBase64(t *testing.T) {
+	sut := core.NewPDF([]byte{1, 2, 3}, nil)
+	b64 := sut.GetBase64()
+
+	assert.Equal(t, "AQID", b64)
+}
+
+func TestPdf_GetBytes(t *testing.T) {
+	sut := core.NewPDF([]byte{1, 2, 3}, nil)
+	bytes := sut.GetBytes()
+
+	assert.Equal(t, []byte{1, 2, 3}, bytes)
+}
