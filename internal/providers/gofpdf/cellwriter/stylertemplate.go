00bb035389ba300768f1b72a8d09c721e8492ee4
pchchv <ipchchv@gmail.com>
2024-10-27 10:12:23 +0300

    Implement SetNext and GoToNext methods

--- internal/providers/gofpdf/cellwriter/stylertemplate.go
+++ internal/providers/gofpdf/cellwriter/stylertemplate.go
@@ -1,6 +1,10 @@
 package cellwriter
 
-import "github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+import (
+	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+	"github.com/pchchv/bpdf/properties"
+)
 
 type stylerTemplate struct {
 	next CellWriter
@@ -14,4 +18,15 @@ func (s *stylerTemplate) GetName() string {
 
 func (s *stylerTemplate) GetNext() CellWriter {
 	return s.next
+}
+
+func (s *stylerTemplate) SetNext(next CellWriter) {
+	s.next = next
+}
+
+func (s *stylerTemplate) GoToNext(width, height float64, config *entity.Config, prop *properties.Cell) {
+	if s.next == nil {
+		return
+	}
+	s.next.Apply(width, height, config, prop)
 }
