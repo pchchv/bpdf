78f3b4bd2106a475c87cc0449f5896f52614c126
pchchv <ipchchv@gmail.com>
2024-10-27 10:13:18 +0300

    Implemnent a cellWriter type

--- internal/providers/gofpdf/cellwriter/cellwriter.go
+++ internal/providers/gofpdf/cellwriter/cellwriter.go
@@ -3,10 +3,16 @@ package cellwriter
 import (
 	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/properties"
+)
 
 type CellWriter interface {
 	SetNext(next CellWriter)
 	GetNext() CellWriter
 	GetName() string
 	Apply(width, height float64, config *entity.Config, prop *properties.Cell)
+}
+
+type cellWriter struct {
+	stylerTemplate
+	defaultColor *properties.Color
 }
