cfc9aa1920f11e22d1d7e0c6d9bd5dce71f741f1
pchchv <ipchchv@gmail.com>
2024-10-28 11:14:24 +0300

    Implement SetProtection, SetMetadata and SetCompression methods

--- internal/providers/gofpdf/provider.go
+++ internal/providers/gofpdf/provider.go
@@ -65,6 +65,47 @@ func (g *provider) CreateCol(width, height float64, config *entity.Config, prop 
 	g.cellWriter.Apply(width, height, config, prop)
 }
 
+func (g *provider) SetProtection(protection *entity.Protection) {
+	if protection == nil {
+		return
+	}
+	g.fpdf.SetProtection(byte(protection.Type), protection.UserPassword, protection.OwnerPassword)
+}
+
+func (g *provider) SetMetadata(metadata *entity.Metadata) {
+	if metadata == nil {
+		return
+	}
+
+	if metadata.Author != nil {
+		g.fpdf.SetAuthor(metadata.Author.Text, metadata.Author.UTF8)
+	}
+
+	if metadata.Creator != nil {
+		g.fpdf.SetCreator(metadata.Creator.Text, metadata.Creator.UTF8)
+	}
+
+	if metadata.Subject != nil {
+		g.fpdf.SetSubject(metadata.Subject.Text, metadata.Subject.UTF8)
+	}
+
+	if metadata.Title != nil {
+		g.fpdf.SetTitle(metadata.Title.Text, metadata.Title.UTF8)
+	}
+
+	if metadata.CreationDate != nil {
+		g.fpdf.SetCreationDate(*metadata.CreationDate)
+	}
+
+	if metadata.KeywordsStr != nil {
+		g.fpdf.SetKeywords(metadata.KeywordsStr.Text, metadata.KeywordsStr.UTF8)
+	}
+}
+
+func (g *provider) SetCompression(compression bool) {
+	g.fpdf.SetCompression(compression)
+}
+
 // loadImage is responsible for loading an codes
 func (g *provider) loadCode(code, codeType string, generate func(code string) (*entity.Image, error)) (*entity.Image, error) {
 	image, err := g.cache.GetImage(codeType+code, extension.Png)
