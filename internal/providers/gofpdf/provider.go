6a17a2450ccb006728670cdd352ed7b3d51c0cf1
pchchv <ipchchv@gmail.com>
2024-10-28 11:07:38 +0300

    Implement an AddLine and a GetFontHeight methods

--- internal/providers/gofpdf/provider.go
+++ internal/providers/gofpdf/provider.go
@@ -9,6 +9,25 @@ import (
 	"github.com/pchchv/bpdf/properties"
 )
 
+// import (
+// 	"bytes"
+// 	"errors"
+// 	"fmt"
+// 	"path/filepath"
+// 	"strings"
+
+// 	"github.com/johnfercher/maroto/v2/pkg/consts/barcode"
+
+// 	"github.com/johnfercher/maroto/v2/internal/cache"
+// 	"github.com/johnfercher/maroto/v2/internal/merror"
+// 	"github.com/johnfercher/maroto/v2/internal/providers/gofpdf/cellwriter"
+// 	"github.com/johnfercher/maroto/v2/internal/providers/gofpdf/fpdfwrapper"
+// 	"github.com/johnfercher/maroto/v2/pkg/consts/extension"
+// 	"github.com/johnfercher/maroto/v2/pkg/core"
+// 	"github.com/johnfercher/maroto/v2/pkg/core/entity"
+// 	"github.com/johnfercher/maroto/v2/pkg/props"
+// )
+
 type provider struct {
 	fpdf       fpdfwrapper.Fpdf
 	font       core.Font
@@ -21,10 +40,18 @@ type provider struct {
 	cfg        *entity.Config
 }
 
+func (g *provider) GetLinesQuantity(text string, textProp *properties.Text, colWidth float64) int {
+	return g.text.GetLinesQuantity(text, textProp, colWidth)
+}
+
+func (g *provider) GetFontHeight(prop *properties.Font) float64 {
+	return g.font.GetHeight(prop.Family, prop.Style, prop.Size)
+}
+
 func (g *provider) AddText(text string, cell *entity.Cell, prop *properties.Text) {
 	g.text.Add(text, cell, prop)
 }
 
-func (g *provider) GetLinesQuantity(text string, textProp *properties.Text, colWidth float64) int {
-	return g.text.GetLinesQuantity(text, textProp, colWidth)
+func (g *provider) AddLine(cell *entity.Cell, prop *properties.Line) {
+	g.line.Add(cell, prop)
 }
