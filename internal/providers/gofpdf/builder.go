13faac8c09c5e079b8c2894b4853d6fb98e2a4b5
pchchv <ipchchv@gmail.com>
2024-10-28 11:36:10 +0300

    Implement a Dependencies type

---
+++ internal/providers/gofpdf/builder.go
@@ -0,0 +1,22 @@
+package gofpdf
+
+import (
+	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/internal/cache"
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/cellwriter"
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+)
+
+// Dependencies is the dependencies provider for gofpdf
+type Dependencies struct {
+	Fpdf       fpdfwrapper.Fpdf
+	Font       core.Font
+	Text       core.Text
+	Code       core.Code
+	Image      core.Image
+	Line       core.Line
+	Cache      cache.Cache
+	CellWriter cellwriter.CellWriter
+	Cfg        *entity.Config
+}
