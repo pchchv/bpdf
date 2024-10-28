d2c5f1483d50861a158f795aaaa44aa994fb71a2
pchchv <ipchchv@gmail.com>
2024-10-28 11:59:16 +0300

    Create tests of SetFamily, SetStyle and SetSize

--- internal/providers/gofpdf/font_test.go
+++ internal/providers/gofpdf/font_test.go
@@ -41,3 +41,46 @@ func TestFont_GetHeight(t *testing.T) {
 
 	assert.Equal(t, 3.527777777777778, height)
 }
+
+func TestFont_SetFamily(t *testing.T) {
+	size := 10.0
+	family := fontfamily.Arial
+	style := fontstyle.Bold
+	fpdf := mocks.NewFpdf(t)
+	fpdf.EXPECT().SetFont(family, string(style), size)
+	fpdf.EXPECT().SetFont(fontfamily.Helvetica, string(style), size)
+	font := gofpdf.NewFont(fpdf, size, family, style)
+
+	font.SetFamily(fontfamily.Helvetica)
+
+	assert.Equal(t, fontfamily.Helvetica, font.GetFamily())
+}
+
+func TestFont_SetStyle(t *testing.T) {
+	size := 10.0
+	family := fontfamily.Arial
+	style := fontstyle.Bold
+
+	fpdf := mocks.NewFpdf(t)
+	fpdf.EXPECT().SetFont(family, string(style), size)
+	fpdf.EXPECT().SetFontStyle(string(fontstyle.BoldItalic))
+	font := gofpdf.NewFont(fpdf, size, family, style)
+
+	font.SetStyle(fontstyle.BoldItalic)
+
+	assert.Equal(t, fontstyle.BoldItalic, font.GetStyle())
+}
+
+func TestFont_SetSize(t *testing.T) {
+	size := 10.0
+	family := fontfamily.Arial
+	style := fontstyle.Bold
+	fpdf := mocks.NewFpdf(t)
+	fpdf.EXPECT().SetFont(family, string(style), size)
+	fpdf.EXPECT().SetFontSize(14.0)
+	font := gofpdf.NewFont(fpdf, size, family, style)
+
+	font.SetSize(14.0)
+
+	assert.Equal(t, 14.0, font.GetSize())
+}
