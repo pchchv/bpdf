dedc4e48147e3ac38db24452175f98a5d59b5bd0
pchchv <ipchchv@gmail.com>
2024-10-28 11:41:47 +0300

    Implement a NewFont function

--- internal/providers/gofpdf/font.go
+++ internal/providers/gofpdf/font.go
@@ -6,6 +6,11 @@ import (
 	"github.com/pchchv/bpdf/properties"
 )
 
+const (
+	gofpdfFontScale1 = 72.0
+	gofpdfFontScale2 = 25.4
+)
+
 type font struct {
 	pdf         fpdfwrapper.Fpdf
 	size        float64
@@ -13,4 +18,17 @@ type font struct {
 	style       fontstyle.Fontstyle
 	scaleFactor float64
 	fontColor   *properties.Color
+}
+
+// NewFont create a Font.
+func NewFont(pdf fpdfwrapper.Fpdf, size float64, family string, style fontstyle.Fontstyle) *font {
+	pdf.SetFont(family, string(style), size)
+	return &font{
+		pdf:         pdf,
+		size:        size,
+		family:      family,
+		style:       style,
+		scaleFactor: gofpdfFontScale1 / gofpdfFontScale2, // Bytes defined inside gofpdf constructor,
+		fontColor:   &properties.Color{Red: 0, Green: 0, Blue: 0},
+	}
 }
