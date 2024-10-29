ad945e39ede0c5bc50318af614ad2d4c88807b16
pchchv <ipchchv@gmail.com>
2024-10-29 11:58:36 +0300

    Implement a method Resize

--- internal/math/math.go
+++ internal/math/math.go
@@ -1,8 +1,35 @@
 package math
 
+import "github.com/pchchv/bpdf/core/entity"
+
 type math struct{}
 
 // New create a Math.
 func New() *math {
 	return &math{}
+}
+
+// Resize adjusts the internal dimension of an element to occupy a percentage of the available space
+//   - inner: The inner dimensions of the element
+//   - outer: The outer dimensions of the element
+//   - percent: The percentage of the external dimension that can be occupied
+//   - justReferenceWidth: Indicates whether resizing should be done only in relation to width or in relation to width and height
+func (s *math) Resize(inner *entity.Dimensions, outer *entity.Dimensions, percent float64, justReferenceWidth bool) *entity.Dimensions {
+	percent /= 100.0
+	innerProportion := inner.Height / inner.Width
+	outerProportion := outer.Height / outer.Width
+	newInnerWidth := 0.0
+	if innerProportion > outerProportion && !justReferenceWidth {
+		newInnerWidth = outer.Height / innerProportion * percent
+	} else {
+		newInnerWidth = outer.Width * percent
+	}
+
+	newInnerHeight := newInnerWidth * innerProportion
+	if justReferenceWidth && newInnerHeight > outer.Height {
+		newInnerWidth = outer.Height / innerProportion * 1
+		newInnerHeight = newInnerWidth * innerProportion
+	}
+
+	return &entity.Dimensions{Width: newInnerWidth, Height: newInnerHeight}
 }
