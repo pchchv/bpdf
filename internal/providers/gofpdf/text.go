55885a3219eaac4582b8ac25529729ce397522e8
pchchv <ipchchv@gmail.com>
2024-10-29 11:41:59 +0300

    Implement an addLine method

--- internal/providers/gofpdf/text.go
+++ internal/providers/gofpdf/text.go
@@ -1,8 +1,10 @@
 package gofpdf
 
 import (
+	"strings"
 	"unicode"
 
+	"github.com/pchchv/bpdf/consts/align"
 	"github.com/pchchv/bpdf/consts/fontfamily"
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
@@ -35,6 +37,61 @@ func (s *text) textToUnicode(txt string, props *properties.Text) string {
 	}
 
 	return txt
+}
+
+func (s *text) addLine(textProp *properties.Text, xColOffset, colWidth, yColOffset, textWidth float64, text string) {
+	left, top, _, _ := s.pdf.GetMargins()
+	fontHeight := s.font.GetHeight(textProp.Family, textProp.Style, textProp.Size)
+	if textProp.Align == align.Left {
+		s.pdf.Text(xColOffset+left, yColOffset+top, text)
+		if textProp.Hyperlink != nil {
+			s.pdf.LinkString(xColOffset+left, yColOffset+top-fontHeight, textWidth, fontHeight, *textProp.Hyperlink)
+		}
+
+		return
+	}
+
+	if textProp.Align == align.Justify {
+		const spaceString = " "
+		const emptyString = ""
+		text = strings.TrimRight(text, spaceString)
+		textNotSpaces := strings.ReplaceAll(text, spaceString, emptyString)
+		textWidth = s.pdf.GetStringWidth(textNotSpaces)
+		defaultSpaceWidth := s.pdf.GetStringWidth(spaceString)
+		words := strings.Fields(text)
+		numSpaces := max(len(words)-1, 1)
+		spaceWidth := (colWidth - textWidth) / float64(numSpaces)
+		x := xColOffset + left
+		if isIncorrectSpaceWidth(textWidth, spaceWidth, defaultSpaceWidth, textNotSpaces) {
+			spaceWidth = defaultSpaceWidth
+		}
+
+		var finishX float64
+		initX := x
+		for _, word := range words {
+			s.pdf.Text(x, yColOffset+top, word)
+			finishX = x + s.pdf.GetStringWidth(word)
+			x = finishX + spaceWidth
+		}
+
+		if textProp.Hyperlink != nil {
+			s.pdf.LinkString(initX, yColOffset+top-fontHeight, finishX-initX, fontHeight, *textProp.Hyperlink)
+		}
+
+		return
+	}
+
+	modifier := 2.0
+	if textProp.Align == align.Right {
+		modifier = 1
+	}
+
+	dx := (colWidth - textWidth) / modifier
+	if textProp.Hyperlink != nil {
+		s.pdf.LinkString(dx+xColOffset+left, yColOffset+top-fontHeight, textWidth, fontHeight, *textProp.Hyperlink)
+	}
+
+	s.pdf.Text(dx+xColOffset+left, yColOffset+top, text)
 }
 
 func isIncorrectSpaceWidth(textWidth, spaceWidth, defaultSpaceWidth float64, text string) bool {
