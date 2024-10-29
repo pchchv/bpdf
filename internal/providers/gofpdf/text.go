7a195a5566cf2edf58ce99322f4cf6140f6d332b
pchchv <ipchchv@gmail.com>
2024-10-29 11:38:00 +0300

    Implement a textToUnicode method

--- internal/providers/gofpdf/text.go
+++ internal/providers/gofpdf/text.go
@@ -1,8 +1,10 @@
 package gofpdf
 
 import (
+	"github.com/pchchv/bpdf/consts/fontfamily"
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+	"github.com/pchchv/bpdf/properties"
 )
 
 type text struct {
@@ -18,4 +20,17 @@ func NewText(pdf fpdfwrapper.Fpdf, math core.Math, font core.Font) *text {
 		math,
 		font,
 	}
+}
+
+func (s *text) textToUnicode(txt string, props *properties.Text) string {
+	if props.Family == fontfamily.Arial ||
+		props.Family == fontfamily.Helvetica ||
+		props.Family == fontfamily.Symbol ||
+		props.Family == fontfamily.ZapBats ||
+		props.Family == fontfamily.Courier {
+		translator := s.pdf.UnicodeTranslatorFromDescriptor("")
+		return translator(txt)
+	}
+
+	return txt
 }
