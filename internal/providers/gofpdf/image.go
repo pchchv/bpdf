8125dd7eb180b3064e50df8fa1303e8639d5222b
pchchv <ipchchv@gmail.com>
2024-10-29 11:49:43 +0300

    Create image type

---
+++ internal/providers/gofpdf/image.go
@@ -0,0 +1,11 @@
+package gofpdf
+
+import (
+	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+)
+
+type image struct {
+	pdf  fpdfwrapper.Fpdf
+	math core.Math
+}
