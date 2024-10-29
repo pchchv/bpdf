8b03aaf08ee8231e92c856892472503a7f03af63
pchchv <ipchchv@gmail.com>
2024-10-29 11:51:27 +0300

    Implement a GetImageInfo method

--- internal/providers/gofpdf/image.go
+++ internal/providers/gofpdf/image.go
@@ -1,7 +1,13 @@
 package gofpdf
 
 import (
+	"bytes"
+
+	"github.com/google/uuid"
+	"github.com/jung-kurt/gofpdf"
+	"github.com/pchchv/bpdf/consts/extension"
 	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
 )
 
@@ -16,4 +22,18 @@ func NewImage(pdf fpdfwrapper.Fpdf, math core.Math) *image {
 		pdf,
 		math,
 	}
+}
+
+// GetImageInfo is responsible for loading the image in PDF and returning its information
+func (s image) GetImageInfo(img *entity.Image, extension extension.Extension) (*gofpdf.ImageInfoType, uuid.UUID) {
+	imageID, _ := uuid.NewRandom()
+	info := s.pdf.RegisterImageOptionsReader(
+		imageID.String(),
+		gofpdf.ImageOptions{
+			ReadDpi:   false,
+			ImageType: string(extension),
+		},
+		bytes.NewReader(img.Bytes),
+	)
+	return info, imageID
 }
