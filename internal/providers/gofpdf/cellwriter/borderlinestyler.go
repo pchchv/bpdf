63d70de988a502857e9d6ff5916abfc9072f70fa
pchchv <ipchchv@gmail.com>
2024-10-30 09:45:17 +0300

    Implement a method Apply

--- internal/providers/gofpdf/cellwriter/borderlinestyler.go
+++ internal/providers/gofpdf/cellwriter/borderlinestyler.go
@@ -1,6 +1,11 @@
 package cellwriter
 
-import "github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+import (
+	"github.com/pchchv/bpdf/consts/linestyle"
+	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+	"github.com/pchchv/bpdf/properties"
+)
 
 type borderLineStyler struct {
 	stylerTemplate
@@ -13,4 +18,20 @@ func NewBorderLineStyler(fpdf fpdfwrapper.Fpdf) *borderLineStyler {
 			name: "borderLineStyler",
 		},
 	}
+}
+
+func (b *borderLineStyler) Apply(width, height float64, config *entity.Config, prop *properties.Cell) {
+	if prop == nil {
+		b.GoToNext(width, height, config, prop)
+		return
+	}
+
+	if prop.LineStyle == linestyle.Solid || prop.LineStyle == "" {
+		b.GoToNext(width, height, config, prop)
+		return
+	}
+
+	b.fpdf.SetDashPattern([]float64{1, 1}, 0)
+	b.GoToNext(width, height, config, prop)
+	b.fpdf.SetDashPattern([]float64{1, 0}, 0)
 }
