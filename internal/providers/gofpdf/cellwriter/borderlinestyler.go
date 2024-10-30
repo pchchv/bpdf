5f4ed4fbe78b0153510467a2db0860671e1423f0
pchchv <ipchchv@gmail.com>
2024-10-30 09:44:50 +0300

    Implement a NewBorderLineStyler func

--- internal/providers/gofpdf/cellwriter/borderlinestyler.go
+++ internal/providers/gofpdf/cellwriter/borderlinestyler.go
@@ -1,5 +1,16 @@
 package cellwriter
 
+import "github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+
 type borderLineStyler struct {
 	stylerTemplate
+}
+
+func NewBorderLineStyler(fpdf fpdfwrapper.Fpdf) *borderLineStyler {
+	return &borderLineStyler{
+		stylerTemplate: stylerTemplate{
+			fpdf: fpdf,
+			name: "borderLineStyler",
+		},
+	}
 }
