375aa79a21725a243cfbc2a792239f187cc7f2b5
pchchv <ipchchv@gmail.com>
2024-10-30 09:39:32 +0300

    Implement a method Apply

--- internal/providers/gofpdf/cellwriter/bordercolorstyler.go
+++ internal/providers/gofpdf/cellwriter/bordercolorstyler.go
@@ -1,6 +1,7 @@
 package cellwriter
 
 import (
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -18,4 +19,20 @@ func NewBorderColorStyler(fpdf fpdfwrapper.Fpdf) *borderColorStyler {
 		},
 		defaultColor: &properties.BlackColor,
 	}
+}
+
+func (b *borderColorStyler) Apply(width, height float64, config *entity.Config, prop *properties.Cell) {
+	if prop == nil {
+		b.GoToNext(width, height, config, prop)
+		return
+	}
+
+	if prop.BorderColor == nil {
+		b.GoToNext(width, height, config, prop)
+		return
+	}
+
+	b.fpdf.SetDrawColor(prop.BorderColor.Red, prop.BorderColor.Green, prop.BorderColor.Blue)
+	b.GoToNext(width, height, config, prop)
+	b.fpdf.SetDrawColor(b.defaultColor.Red, b.defaultColor.Green, b.defaultColor.Blue)
 }
