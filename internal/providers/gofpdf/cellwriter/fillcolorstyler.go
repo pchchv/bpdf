ec5fd17e71f4ed1fed044791110bf101d8036c00
pchchv <ipchchv@gmail.com>
2024-10-30 09:55:43 +0300

    Implement a method Apply

--- internal/providers/gofpdf/cellwriter/fillcolorstyler.go
+++ internal/providers/gofpdf/cellwriter/fillcolorstyler.go
@@ -1,6 +1,7 @@
 package cellwriter
 
 import (
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -18,4 +19,20 @@ func NewFillColorStyler(fpdf fpdfwrapper.Fpdf) *fillColorStyler {
 		},
 		defaultFillColor: &properties.WhiteColor,
 	}
+}
+
+func (f *fillColorStyler) Apply(width, height float64, config *entity.Config, prop *properties.Cell) {
+	if prop == nil {
+		f.GoToNext(width, height, config, prop)
+		return
+	}
+
+	if prop.BackgroundColor == nil {
+		f.GoToNext(width, height, config, prop)
+		return
+	}
+
+	f.fpdf.SetFillColor(prop.BackgroundColor.Red, prop.BackgroundColor.Green, prop.BackgroundColor.Blue)
+	f.GoToNext(width, height, config, prop)
+	f.fpdf.SetFillColor(f.defaultFillColor.Red, f.defaultFillColor.Green, f.defaultFillColor.Blue)
 }
