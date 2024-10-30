1d52532ce97b3adb3caa5552c5a0b7d38f5efd81
pchchv <ipchchv@gmail.com>
2024-10-30 09:58:13 +0300

    Implement a method Build

--- internal/providers/gofpdf/cellwriter/builder.go
+++ internal/providers/gofpdf/cellwriter/builder.go
@@ -1,7 +1,24 @@
 package cellwriter
 
+import "github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+
 type CellWriterBuilder struct{}
 
 func NewBuilder() *CellWriterBuilder {
 	return &CellWriterBuilder{}
+}
+
+func (c *CellWriterBuilder) Build(fpdf fpdfwrapper.Fpdf) CellWriter {
+	cellCreator := NewCellWriter(fpdf)
+	borderColorStyle := NewBorderColorStyler(fpdf)
+	borderLineStyler := NewBorderLineStyler(fpdf)
+	borderThicknessStyler := NewBorderThicknessStyler(fpdf)
+	fillColorStyler := NewFillColorStyler(fpdf)
+
+	borderThicknessStyler.SetNext(borderLineStyler)
+	borderLineStyler.SetNext(borderColorStyle)
+	borderColorStyle.SetNext(fillColorStyler)
+	fillColorStyler.SetNext(cellCreator)
+
+	return borderThicknessStyler
 }
