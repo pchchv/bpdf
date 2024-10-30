12aecd60be1a0061f88b09e763eaff1b895bf697
pchchv <ipchchv@gmail.com>
2024-10-30 09:59:01 +0300

    Create tests of a Build method

--- internal/providers/gofpdf/cellwriter/builder_test.go
+++ internal/providers/gofpdf/cellwriter/builder_test.go
@@ -14,3 +14,21 @@ func TestNewBuilder(t *testing.T) {
 	assert.NotNil(t, sut)
 	assert.Equal(t, "*cellwriter.CellWriterBuilder", fmt.Sprintf("%T", sut))
 }
+
+func TestCellWriterBuilder_Build(t *testing.T) {
+	sut := cellwriter.NewBuilder()
+
+	chain := sut.Build(nil)
+
+	assert.Equal(t, "borderThicknessStyler", chain.GetName())
+	chain = chain.GetNext()
+	assert.Equal(t, "borderLineStyler", chain.GetName())
+	chain = chain.GetNext()
+	assert.Equal(t, "borderColorStyler", chain.GetName())
+	chain = chain.GetNext()
+	assert.Equal(t, "fillColorStyler", chain.GetName())
+	chain = chain.GetNext()
+	assert.Equal(t, "cellWriter", chain.GetName())
+	chain = chain.GetNext()
+	assert.Nil(t, chain)
+}
