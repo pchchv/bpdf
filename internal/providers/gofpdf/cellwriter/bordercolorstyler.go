db9a0f0910ebaa9d2b30d15d9a051480a4d25c7a
pchchv <ipchchv@gmail.com>
2024-10-30 09:39:08 +0300

    Implement a NewBorderColorStyler func

--- internal/providers/gofpdf/cellwriter/bordercolorstyler.go
+++ internal/providers/gofpdf/cellwriter/bordercolorstyler.go
@@ -1,8 +1,21 @@
 package cellwriter
 
-import "github.com/pchchv/bpdf/properties"
+import (
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+	"github.com/pchchv/bpdf/properties"
+)
 
 type borderColorStyler struct {
 	stylerTemplate
 	defaultColor *properties.Color
+}
+
+func NewBorderColorStyler(fpdf fpdfwrapper.Fpdf) *borderColorStyler {
+	return &borderColorStyler{
+		stylerTemplate: stylerTemplate{
+			fpdf: fpdf,
+			name: "borderColorStyler",
+		},
+		defaultColor: &properties.BlackColor,
+	}
 }
