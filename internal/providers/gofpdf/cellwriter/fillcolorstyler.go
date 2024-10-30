455b3cb8aa7020088b0fe8c038c7d57c06567bbe
pchchv <ipchchv@gmail.com>
2024-10-30 09:55:28 +0300

    Implement a NewFillColorStyler func

--- internal/providers/gofpdf/cellwriter/fillcolorstyler.go
+++ internal/providers/gofpdf/cellwriter/fillcolorstyler.go
@@ -1,8 +1,21 @@
 package cellwriter
 
-import "github.com/pchchv/bpdf/properties"
+import (
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+	"github.com/pchchv/bpdf/properties"
+)
 
 type fillColorStyler struct {
 	stylerTemplate
 	defaultFillColor *properties.Color
+}
+
+func NewFillColorStyler(fpdf fpdfwrapper.Fpdf) *fillColorStyler {
+	return &fillColorStyler{
+		stylerTemplate: stylerTemplate{
+			fpdf: fpdf,
+			name: "fillColorStyler",
+		},
+		defaultFillColor: &properties.WhiteColor,
+	}
 }
