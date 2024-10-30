4dbc6f04deb2a134edaa796de45716415e33230f
pchchv <ipchchv@gmail.com>
2024-10-30 09:06:34 +0300

    Implement a NewLine func

--- internal/providers/gofpdf/line.go
+++ internal/providers/gofpdf/line.go
@@ -1,6 +1,7 @@
 package gofpdf
 
 import (
+	"github.com/pchchv/bpdf/consts/linestyle"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -9,4 +10,12 @@ type line struct {
 	pdf              fpdfwrapper.Fpdf
 	defaultColor     *properties.Color
 	defaultThickness float64
+}
+
+func NewLine(pdf fpdfwrapper.Fpdf) *line {
+	return &line{
+		pdf:              pdf,
+		defaultColor:     &properties.BlackColor,
+		defaultThickness: linestyle.DefaultLineThickness,
+	}
 }
