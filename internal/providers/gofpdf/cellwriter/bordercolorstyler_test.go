f697c5e8a24e2d92018c3b59e40760de4a49101b
pchchv <ipchchv@gmail.com>
2024-10-30 09:41:21 +0300

    fix

--- internal/providers/gofpdf/cellwriter/bordercolorstyler_test.go
+++ internal/providers/gofpdf/cellwriter/bordercolorstyler_test.go
@@ -1,3 +1,12 @@
+package cellwriter_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/cellwriter"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewBorderColorStyler(t *testing.T) {
 	sut := cellwriter.NewBorderColorStyler(nil)
