983008e20d1c88c47508f8cc0467b23d57ad418b
pchchv <ipchchv@gmail.com>
2024-10-30 09:49:51 +0300

    Implement a method Apply

--- internal/providers/gofpdf/cellwriter/borderthicknessstyler.go
+++ internal/providers/gofpdf/cellwriter/borderthicknessstyler.go
@@ -2,7 +2,9 @@ package cellwriter
 
 import (
 	"github.com/pchchv/bpdf/consts/linestyle"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+	"github.com/pchchv/bpdf/properties"
 )
 
 type borderThicknessStyler struct {
@@ -18,4 +20,20 @@ func NewBorderThicknessStyler(fpdf fpdfwrapper.Fpdf) *borderThicknessStyler {
 		},
 		defaultLineThickness: linestyle.DefaultLineThickness,
 	}
+}
+
+func (b *borderThicknessStyler) Apply(width, height float64, config *entity.Config, prop *properties.Cell) {
+	if prop == nil {
+		b.GoToNext(width, height, config, prop)
+		return
+	}
+
+	if prop.BorderThickness == 0 {
+		b.GoToNext(width, height, config, prop)
+		return
+	}
+
+	b.fpdf.SetLineWidth(prop.BorderThickness)
+	b.GoToNext(width, height, config, prop)
+	b.fpdf.SetLineWidth(b.defaultLineThickness)
 }
