63f51ce40f84b1c8d6475a63ee494397d905c941
pchchv <ipchchv@gmail.com>
2024-10-30 09:49:27 +0300

    Implement a NewBorderThicknessStyler func

--- internal/providers/gofpdf/cellwriter/borderthicknessstyler.go
+++ internal/providers/gofpdf/cellwriter/borderthicknessstyler.go
@@ -1,6 +1,21 @@
 package cellwriter
 
+import (
+	"github.com/pchchv/bpdf/consts/linestyle"
+	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
+)
+
 type borderThicknessStyler struct {
 	stylerTemplate
 	defaultLineThickness float64
+}
+
+func NewBorderThicknessStyler(fpdf fpdfwrapper.Fpdf) *borderThicknessStyler {
+	return &borderThicknessStyler{
+		stylerTemplate: stylerTemplate{
+			fpdf: fpdf,
+			name: "borderThicknessStyler",
+		},
+		defaultLineThickness: linestyle.DefaultLineThickness,
+	}
 }
