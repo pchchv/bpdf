56519a85a8e810d3dae07bff286d2948d6e8eba1
pchchv <ipchchv@gmail.com>
2024-10-31 12:50:05 +0300

    Implement a method Build

--- internal/providers/gofpdf/builder.go
+++ internal/providers/gofpdf/builder.go
@@ -1,9 +1,12 @@
 package gofpdf
 
 import (
+	"github.com/jung-kurt/gofpdf"
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/cache"
+	"github.com/pchchv/bpdf/internal/code"
+	"github.com/pchchv/bpdf/internal/math"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/cellwriter"
 	"github.com/pchchv/bpdf/internal/providers/gofpdf/fpdfwrapper"
 )
@@ -31,4 +34,51 @@ type builder struct{}
 // NewBuilder create a new Builder
 func NewBuilder() *builder {
 	return &builder{}
+}
+
+// Build create a new Dependencies
+func (b *builder) Build(cfg *entity.Config, cache cache.Cache) *Dependencies {
+	fpdf := fpdfwrapper.NewCustom(&gofpdf.InitType{
+		OrientationStr: "P",
+		UnitStr:        "mm",
+		FontDirStr:     "",
+		Size: gofpdf.SizeType{
+			Wd: cfg.Dimensions.Width,
+			Ht: cfg.Dimensions.Height,
+		},
+	})
+
+	for _, font := range cfg.CustomFonts {
+		fpdf.AddUTF8FontFromBytes(font.Family, string(font.Style), font.Bytes)
+	}
+
+	if cfg.DisableAutoPageBreak {
+		fpdf.SetAutoPageBreak(false, 0)
+	} else {
+		fpdf.SetAutoPageBreak(true, cfg.Margins.Bottom)
+	}
+
+	fpdf.SetMargins(cfg.Margins.Left, cfg.Margins.Top, cfg.Margins.Right)
+	fpdf.AddPage()
+
+	font := NewFont(fpdf, cfg.DefaultFont.Size, cfg.DefaultFont.Family, cfg.DefaultFont.Style)
+	math := math.New()
+	code := code.New()
+	text := NewText(fpdf, math, font)
+	image := NewImage(fpdf, math)
+	line := NewLine(fpdf)
+	cellWriter := cellwriter.NewBuilder().
+		Build(fpdf)
+
+	return &Dependencies{
+		Fpdf:       fpdf,
+		Font:       font,
+		Text:       text,
+		Code:       code,
+		Image:      image,
+		Line:       line,
+		CellWriter: cellWriter,
+		Cfg:        cfg,
+		Cache:      cache,
+	}
 }
