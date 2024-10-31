353419e9a130ae5d716f9e172a61c6f5faa25916
pchchv <ipchchv@gmail.com>
2024-10-31 12:09:10 +0300

    Implement a method getImage

--- internal/code/code.go
+++ internal/code/code.go
@@ -1,5 +1,16 @@
 package code
 
+import (
+	"bytes"
+	"image"
+	"image/color/palette"
+	"image/draw"
+	"image/png"
+
+	"github.com/pchchv/bpdf/consts/extension"
+	"github.com/pchchv/bpdf/core/entity"
+)
+
 // codeInstance is a singleton of code.
 // It is used to ensure that it is not instantiated more than once,
 // as it is not necessary since the code is stateless.
@@ -13,4 +24,25 @@ func New() *code {
 		codeInstance = &code{}
 	}
 	return codeInstance
+}
+
+func (c *code) getImage(img image.Image) (*entity.Image, error) {
+	var buf bytes.Buffer
+	dst := image.NewPaletted(img.Bounds(), palette.Plan9)
+	drawer := draw.Drawer(draw.Src)
+	drawer.Draw(dst, dst.Bounds(), img, img.Bounds().Min)
+	if err := png.Encode(&buf, dst); err != nil {
+		return nil, err
+	}
+
+	imgEntity := &entity.Image{
+		Bytes:     buf.Bytes(),
+		Extension: extension.Png,
+		Dimensions: &entity.Dimensions{
+			Width:  float64(dst.Bounds().Dx()),
+			Height: float64(dst.Bounds().Dy()),
+		},
+	}
+
+	return imgEntity, nil
 }
