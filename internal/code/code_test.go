9854758b9fdfb0360e15537f87af30022ef06401
pchchv <ipchchv@gmail.com>
2024-10-31 12:48:34 +0300

    Create tests of GenQr and GenBar methods

--- internal/code/code_test.go
+++ internal/code/code_test.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/pchchv/bpdf/consts/barcode"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/code"
+	"github.com/pchchv/bpdf/properties"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,6 +25,85 @@ func TestNew(t *testing.T) {
 
 		assert.NotNil(t, sut1)
 		assert.NotNil(t, sut2)
+	})
+}
+
+func TestCode_GenQr(t *testing.T) {
+	t.Run("When cannot generate qr code, should return error", func(t *testing.T) {
+		sut := code.New()
+		data := genStringWithLength(5000)
+
+		bytes, err := sut.GenQr(data)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, bytes)
+	})
+
+	t.Run("When can generate qr code, should return bytes", func(t *testing.T) {
+		sut := code.New()
+		data := genStringWithLength(50)
+
+		bytes, err := sut.GenQr(data)
+
+		assert.NotNil(t, bytes)
+		assert.Nil(t, err)
+	})
+}
+
+func TestCode_GenBar(t *testing.T) {
+	t.Run("When cannot generate bar code, should return error", func(t *testing.T) {
+		sut := code.New()
+		cell := &entity.Cell{
+			X:      10,
+			Y:      10,
+			Width:  100,
+			Height: 100,
+		}
+		prop := &properties.Barcode{}
+		prop.MakeValid()
+		data := genStringWithLength(5000)
+
+		bytes, err := sut.GenBar(data, cell, prop)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, bytes)
+	})
+
+	t.Run("When can generate bar code, should return bytes", func(t *testing.T) {
+		sut := code.New()
+		cell := &entity.Cell{
+			X:      10,
+			Y:      10,
+			Width:  100,
+			Height: 100,
+		}
+		prop := &properties.Barcode{}
+		prop.MakeValid()
+		data := genStringWithLength(60)
+
+		bytes, err := sut.GenBar(data, cell, prop)
+
+		assert.NotNil(t, bytes)
+		assert.Nil(t, err)
+	})
+
+	t.Run("When is ean and can generate bar code, should return bytes", func(t *testing.T) {
+		sut := code.New()
+		cell := &entity.Cell{
+			X:      10,
+			Y:      10,
+			Width:  100,
+			Height: 100,
+		}
+		prop := &properties.Barcode{
+			Type: barcode.EAN,
+		}
+		prop.MakeValid()
+
+		bytes, err := sut.GenBar("123456789123", cell, prop)
+
+		assert.NotNil(t, bytes)
+		assert.Nil(t, err)
 	})
 }
 
