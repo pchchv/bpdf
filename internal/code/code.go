311f647740b426dbdb54e9c618d5d1bcede4a61b
pchchv <ipchchv@gmail.com>
2024-10-31 12:07:53 +0300

    Implement a New func

--- internal/code/code.go
+++ internal/code/code.go
@@ -1,3 +1,16 @@
 package code
 
+// codeInstance is a singleton of code.
+// It is used to ensure that it is not instantiated more than once,
+// as it is not necessary since the code is stateless.
+var codeInstance *code = nil
+
 type code struct{}
+
+// New create a Code (Singleton).
+func New() *code {
+	if codeInstance == nil {
+		codeInstance = &code{}
+	}
+	return codeInstance
+}
