6773a79ad3950c3721a5c246350ee75dc978602c
pchchv <ipchchv@gmail.com>
2024-10-31 12:45:26 +0300

    Create test of New func

---
+++ internal/code/code_test.go
@@ -0,0 +1,26 @@
+package code_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/pchchv/bpdf/internal/code"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("constructor", func(t *testing.T) {
+		sut := code.New()
+
+		assert.NotNil(t, sut)
+		assert.Equal(t, "*code.code", fmt.Sprintf("%T", sut))
+	})
+
+	t.Run("singleton is applied", func(t *testing.T) {
+		sut1 := code.New()
+		sut2 := code.New()
+
+		assert.NotNil(t, sut1)
+		assert.NotNil(t, sut2)
+	})
+}
