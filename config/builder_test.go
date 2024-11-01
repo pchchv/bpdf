abea214becef45bd998dc2de1e74c1ec7f56ec56
pchchv <ipchchv@gmail.com>
2024-11-01 17:10:32 +0300

    Create tests of methods Build and WithKeywords

--- config/builder_test.go
+++ config/builder_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 
 	"github.com/pchchv/bpdf/config"
+	"github.com/pchchv/bpdf/consts/fontfamily"
+	"github.com/pchchv/bpdf/consts/fontstyle"
+	"github.com/pchchv/bpdf/consts/generation"
+	"github.com/pchchv/bpdf/consts/provider"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,4 +17,53 @@ func TestNewBuilder(t *testing.T) {
 
 	assert.NotNil(t, sut)
 	assert.Equal(t, "*config.CfgBuilder", fmt.Sprintf("%T", sut))
+}
+
+func TestBuilder_Build(t *testing.T) {
+	sut := config.NewBuilder()
+
+	cfg := sut.Build()
+
+	assert.Equal(t, provider.Gofpdf, cfg.ProviderType)
+	assert.Equal(t, 210.0, cfg.Dimensions.Width)
+	assert.Equal(t, 297.0, cfg.Dimensions.Height)
+	assert.Equal(t, 10.0, cfg.Margins.Top)
+	assert.Equal(t, 10.0, cfg.Margins.Left)
+	assert.Equal(t, 10.0, cfg.Margins.Right)
+	assert.Equal(t, fontfamily.Arial, cfg.DefaultFont.Family)
+	assert.Equal(t, 10.0, cfg.DefaultFont.Size)
+	assert.Equal(t, fontstyle.Normal, cfg.DefaultFont.Style)
+	assert.Equal(t, 0, cfg.DefaultFont.Color.Red)
+	assert.Equal(t, 0, cfg.DefaultFont.Color.Green)
+	assert.Equal(t, 0, cfg.DefaultFont.Color.Blue)
+	assert.Nil(t, cfg.CustomFonts)
+	assert.Equal(t, generation.Sequential, cfg.GenerationMode)
+	assert.Equal(t, 1, cfg.ChunkWorkers)
+	assert.Equal(t, false, cfg.Debug)
+	assert.Equal(t, 12, cfg.MaxGridSize)
+	assert.Nil(t, cfg.PageNumber)
+	assert.Nil(t, cfg.Protection)
+	assert.False(t, cfg.Compression)
+	assert.Nil(t, cfg.Metadata)
+	assert.Nil(t, cfg.BackgroundImage)
+	assert.False(t, cfg.DisableAutoPageBreak)
+}
+
+func TestBuilder_WithKeywords(t *testing.T) {
+	t.Run("when keywords is empty, should ignore", func(t *testing.T) {
+		sut := config.NewBuilder()
+
+		cfg := sut.WithKeywords("", true).Build()
+
+		assert.Nil(t, cfg.Metadata)
+	})
+
+	t.Run("when author valid, should apply", func(t *testing.T) {
+		sut := config.NewBuilder()
+
+		cfg := sut.WithKeywords("keyword", true).Build()
+
+		assert.Equal(t, "keyword", cfg.Metadata.KeywordsStr.Text)
+		assert.Equal(t, true, cfg.Metadata.KeywordsStr.UTF8)
+	})
 }
