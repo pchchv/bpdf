c3cbf92c8f8b2b3bf804a0238b30138a0e8ff074
pchchv <ipchchv@gmail.com>
2024-11-01 19:01:55 +0300

    Implement AddImage and GetImage methods

--- internal/cache/mutexcache.go
+++ internal/cache/mutexcache.go
@@ -1,8 +1,26 @@
 package cache
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/pchchv/bpdf/consts/extension"
+	"github.com/pchchv/bpdf/core/entity"
+)
 
 type mutexCache struct {
 	inner      Cache
 	imageMutex sync.RWMutex
+}
+
+// AddImage adds a behavior to lock/unlock cache write.
+func (c *mutexCache) AddImage(value string, image *entity.Image) {
+	c.imageMutex.Lock()
+	defer c.imageMutex.Unlock()
+
+	c.inner.AddImage(value, image)
+}
+
+// GetImage adds a behavior to lock/unlock cache read.
+func (c *mutexCache) GetImage(file string, extension extension.Extension) (*entity.Image, error) {
+	return c.inner.GetImage(file, extension)
 }
