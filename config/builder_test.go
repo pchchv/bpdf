4b958e56510efe0118f2dd3fd82488d153796ccc
pchchv <ipchchv@gmail.com>
2024-11-01 17:13:36 +0300

    Tests of WithCustomFonts, WithPageSize and WithCreationDate methods

--- config/builder_test.go
+++ config/builder_test.go
@@ -3,13 +3,16 @@ package config_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/pchchv/bpdf/config"
 	"github.com/pchchv/bpdf/consts/extension"
 	"github.com/pchchv/bpdf/consts/fontfamily"
 	"github.com/pchchv/bpdf/consts/fontstyle"
 	"github.com/pchchv/bpdf/consts/generation"
+	"github.com/pchchv/bpdf/consts/pagesize"
 	"github.com/pchchv/bpdf/consts/provider"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -95,5 +98,60 @@ func TestBuilder_WithDisableAutoPageBreak(t *testing.T) {
 		cfg := sut.WithDisableAutoPageBreak(true).Build()
 
 		assert.Equal(t, true, cfg.DisableAutoPageBreak)
+	})
+}
+
+func TestCfgBuilder_WithCustomFonts(t *testing.T) {
+	t.Run("when custom font, should apply", func(t *testing.T) {
+		sut := config.NewBuilder()
+		customFonts := []*entity.CustomFont{
+			{
+				Family: "custom",
+			},
+		}
+
+		cfg := sut.WithCustomFonts(customFonts).Build()
+
+		assert.Equal(t, 1, len(cfg.CustomFonts))
+		assert.Equal(t, "custom", cfg.CustomFonts[0].Family)
+	})
+}
+
+func TestBuilder_WithPageSize(t *testing.T) {
+	t.Run("when page size is empty, should not change the default value", func(t *testing.T) {
+		sut := config.NewBuilder()
+
+		cfg := sut.WithPageSize("").Build()
+
+		assert.Equal(t, 210.0, cfg.Dimensions.Width)
+		assert.Equal(t, 297.0, cfg.Dimensions.Height)
+	})
+
+	t.Run("when page size is filled, should change the default value", func(t *testing.T) {
+		sut := config.NewBuilder()
+
+		cfg := sut.WithPageSize(pagesize.A2).Build()
+
+		assert.Equal(t, 419.9, cfg.Dimensions.Width)
+		assert.Equal(t, 594.0, cfg.Dimensions.Height)
+	})
+}
+
+func TestBuilder_WithCreationDate(t *testing.T) {
+	t.Run("when time is zero, should ignore", func(t *testing.T) {
+		sut := config.NewBuilder()
+
+		cfg := sut.WithCreationDate(time.Time{}).Build()
+
+		assert.Nil(t, cfg.Metadata)
+	})
+
+	t.Run("when time valid, should apply", func(t *testing.T) {
+		sut := config.NewBuilder()
+		timeNow := time.Now()
+
+		cfg := sut.WithCreationDate(timeNow).Build()
+
+		assert.Equal(t, &timeNow, cfg.Metadata.CreationDate)
 	})
 }
