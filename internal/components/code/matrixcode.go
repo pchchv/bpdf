faff2676783f707ad1abb3d6aa77164b91039234
pchchv <ipchchv@gmail.com>
2024-11-02 12:08:55 +0300

    Implement SetConfig and Render methods

--- internal/components/code/matrixcode.go
+++ internal/components/code/matrixcode.go
@@ -2,6 +2,7 @@
 package code
 
 import (
+	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -10,4 +11,14 @@ type MatrixCode struct {
 	code   string
 	prop   properties.Rect
 	config *entity.Config
+}
+
+// SetConfig sets the configuration of a MatrixCode.
+func (m *MatrixCode) SetConfig(config *entity.Config) {
+	m.config = config
+}
+
+// Render renders a MatrixCode into a PDF context.
+func (m *MatrixCode) Render(provider core.Provider, cell *entity.Cell) {
+	provider.AddMatrixCode(m.code, cell, &m.prop)
 }
