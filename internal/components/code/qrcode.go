ec326657d8f87fcc14aae1ed44c088e70a67763c
pchchv <ipchchv@gmail.com>
2024-11-02 12:17:24 +0300

    Implement SetConfig and Render methods

--- internal/components/code/qrcode.go
+++ internal/components/code/qrcode.go
@@ -2,6 +2,7 @@
 package code
 
 import (
+	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -10,4 +11,14 @@ type QrCode struct {
 	code   string
 	prop   properties.Rect
 	config *entity.Config
+}
+
+// Render renders a QrCode into a PDF context.
+func (q *QrCode) Render(provider core.Provider, cell *entity.Cell) {
+	provider.AddQrCode(q.code, cell, &q.prop)
+}
+
+// SetConfig set the config for the component.
+func (q *QrCode) SetConfig(config *entity.Config) {
+	q.config = config
 }
