295ba2e1a35b089ffe19463cab084592cb6b8074
pchchv <ipchchv@gmail.com>
2024-11-02 12:10:03 +0300

    Implement GetStructure and GetHeight methods

--- internal/components/code/matrixcode.go
+++ internal/components/code/matrixcode.go
@@ -4,6 +4,7 @@ package code
 import (
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/node"
 	"github.com/pchchv/bpdf/properties"
 )
 
@@ -21,4 +22,27 @@ func (m *MatrixCode) SetConfig(config *entity.Config) {
 // Render renders a MatrixCode into a PDF context.
 func (m *MatrixCode) Render(provider core.Provider, cell *entity.Cell) {
 	provider.AddMatrixCode(m.code, cell, &m.prop)
+}
+
+// GetStructure returns the Structure of a MatrixCode.
+func (m *MatrixCode) GetStructure() *node.Node[core.Structure] {
+	str := core.Structure{
+		Type:    "matrixcode",
+		Value:   m.code,
+		Details: m.prop.ToMap(),
+	}
+
+	return node.New(str)
+}
+
+// GetHeight returns the height that the code will have in the PDF.
+func (m *MatrixCode) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
+	dimensions, err := provider.GetDimensionsByMatrixCode(m.code)
+	if err != nil {
+		return 0
+	}
+
+	proportion := dimensions.Height / dimensions.Width
+	width := (m.prop.Percent / 100) * cell.Width
+	return proportion * width
 }
