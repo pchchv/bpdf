b58d060e08828db9b5c7007acbc3461d92d4bfdf
pchchv <ipchchv@gmail.com>
2024-11-02 12:17:54 +0300

    Implement GetStructure and GetHeight methods

--- internal/components/code/qrcode.go
+++ internal/components/code/qrcode.go
@@ -4,6 +4,7 @@ package code
 import (
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/node"
 	"github.com/pchchv/bpdf/properties"
 )
 
@@ -21,4 +22,27 @@ func (q *QrCode) Render(provider core.Provider, cell *entity.Cell) {
 // SetConfig set the config for the component.
 func (q *QrCode) SetConfig(config *entity.Config) {
 	q.config = config
+}
+
+// GetStructure returns the Structure of a QrCode.
+func (q *QrCode) GetStructure() *node.Node[core.Structure] {
+	str := core.Structure{
+		Type:    "qrcode",
+		Value:   q.code,
+		Details: q.prop.ToMap(),
+	}
+
+	return node.New(str)
+}
+
+// GetHeight returns the height that the QrCode will have in the PDF.
+func (q *QrCode) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
+	dimensions, err := provider.GetDimensionsByQrCode(q.code)
+	if err != nil {
+		return 0
+	}
+
+	proportion := dimensions.Height / dimensions.Width
+	width := (q.prop.Percent / 100) * cell.Width
+	return proportion * width
 }
