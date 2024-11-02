0c7eba76090821d8c1eae419fc9aa69a358177af
pchchv <ipchchv@gmail.com>
2024-11-02 11:59:06 +0300

    Implement SetConfig and Render methods

--- internal/components/code/barcode.go
+++ internal/components/code/barcode.go
@@ -4,6 +4,7 @@
 package code
 
 import (
+	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -12,4 +13,17 @@ type Barcode struct {
 	code   string
 	prop   properties.Barcode
 	config *entity.Config
+}
+
+// Render renders a Barcode into a PDF context.
+// The bpdf cal this method in process to generate the pdf:
+//   - provider: Is the creator provider used to generate the pdf.
+//   - cell: cell represents the space available to draw the component.
+func (b *Barcode) Render(provider core.Provider, cell *entity.Cell) {
+	provider.AddBarCode(b.code, cell, &b.prop)
+}
+
+// SetConfig sets the configuration of a Barcode.
+func (b *Barcode) SetConfig(config *entity.Config) {
+	b.config = config
 }
