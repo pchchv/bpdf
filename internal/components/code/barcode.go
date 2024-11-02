d5345eb2a66d2430f404e992a62dc46a0fa993ee
pchchv <ipchchv@gmail.com>
2024-11-02 11:59:47 +0300

    Implement GetStructure and GetHeight methods

--- internal/components/code/barcode.go
+++ internal/components/code/barcode.go
@@ -6,6 +6,7 @@ package code
 import (
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/node"
 	"github.com/pchchv/bpdf/properties"
 )
 
@@ -26,4 +27,23 @@ func (b *Barcode) Render(provider core.Provider, cell *entity.Cell) {
 // SetConfig sets the configuration of a Barcode.
 func (b *Barcode) SetConfig(config *entity.Config) {
 	b.config = config
+}
+
+// GetStructure returns the structure of a barcode.
+// This method is typically used when creating tests.
+func (b *Barcode) GetStructure() *node.Node[core.Structure] {
+	str := core.Structure{
+		Type:    "barcode",
+		Value:   b.code,
+		Details: b.prop.ToMap(),
+	}
+
+	return node.New(str)
+}
+
+// GetHeight returns the height that the barcode will have in the PDF.
+func (b *Barcode) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
+	proportion := b.prop.Proportion.Height / b.prop.Proportion.Width
+	width := (b.prop.Percent / 100) * cell.Width
+	return proportion * width
 }
