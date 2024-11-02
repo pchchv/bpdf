05ec4e329c590f5cba1b92402d8c4eb164c4074d
pchchv <ipchchv@gmail.com>
2024-11-02 12:29:01 +0300

    Implement a getBiggestCol and a resetHeight methods

--- internal/components/row/row.go
+++ internal/components/row/row.go
@@ -14,3 +14,20 @@ type Row struct {
 	style      *properties.Cell
 	config     *entity.Config
 }
+
+// Returns the height of the row content.
+func (r *Row) getBiggestCol(provider core.Provider, cell *entity.Cell) float64 {
+	greaterHeight := 0.0
+	for _, col := range r.cols {
+		height := col.GetHeight(provider, cell)
+		if greaterHeight < height {
+			greaterHeight = height
+		}
+	}
+	return greaterHeight
+}
+
+// resetHeight resets the line height to 0.
+func (r *Row) resetHeight() {
+	r.height = 0
+}
