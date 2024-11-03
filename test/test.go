9598ec3c4cf81e250fcaa435426b57c8f13dfd78
pchchv <ipchchv@gmail.com>
2024-11-03 09:03:47 +0300

    Create a BPDFTest type

--- test/test.go
+++ test/test.go
@@ -1,8 +1,21 @@
 package test
 
+import (
+	"testing"
+
+	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/node"
+)
+
 type Node struct {
 	Value   interface{}            `json:"value,omitempty"`
 	Type    string                 `json:"type"`
 	Details map[string]interface{} `json:"details,omitempty"`
 	Nodes   []*Node                `json:"nodes,omitempty"`
+}
+
+// BPDFTest is the unit test instance.
+type BPDFTest struct {
+	t    *testing.T
+	node *node.Node[core.Structure]
 }
