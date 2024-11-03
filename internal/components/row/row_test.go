a530410ee2825c43f0c964d10b1bae73c512e701
pchchv <ipchchv@gmail.com>
2024-11-03 08:58:24 +0300

    Create tests of a method GetStructure

--- internal/components/row/row_test.go
+++ internal/components/row/row_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/components/col"
 	"github.com/pchchv/bpdf/internal/components/row"
 	"github.com/pchchv/bpdf/internal/fixture"
@@ -30,5 +31,52 @@ func TestRow_GetColumns(t *testing.T) {
 		r := row.New(10).Add(newCol[0])
 
 		assert.Equal(t, newCol, r.GetColumns())
+	})
+}
+
+func TestRow_GetStructure(t *testing.T) {
+	t.Run("when there is no style, should call provider correctly", func(t *testing.T) {
+		cfg := &entity.Config{
+			MaxGridSize: 12,
+		}
+		cell := fixture.CellEntity()
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().CreateRow(cell.Height)
+		col := mocks.NewCol(t)
+		col.EXPECT().Render(provider, cell, true)
+		col.EXPECT().SetConfig(cfg)
+		col.EXPECT().GetSize().Return(12)
+		sut := row.New(cell.Height).Add(col)
+		sut.SetConfig(cfg)
+
+		sut.Render(provider, cell)
+
+		provider.AssertNumberOfCalls(t, "CreateRow", 1)
+		col.AssertNumberOfCalls(t, "Render", 1)
+		col.AssertNumberOfCalls(t, "SetConfig", 1)
+	})
+
+	t.Run("when there is style, should call provider correctly", func(t *testing.T) {
+		cfg := &entity.Config{
+			MaxGridSize: 12,
+		}
+		cell := fixture.CellEntity()
+		prop := fixture.CellProp()
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().CreateRow(cell.Height)
+		provider.EXPECT().CreateCol(cell.Width, cell.Height, cfg, &prop)
+		col := mocks.NewCol(t)
+		col.EXPECT().Render(provider, cell, false)
+		col.EXPECT().SetConfig(cfg)
+		col.EXPECT().GetSize().Return(12)
+		sut := row.New(cell.Height).Add(col).WithStyle(&prop)
+		sut.SetConfig(cfg)
+
+		sut.Render(provider, cell)
+
+		provider.AssertNumberOfCalls(t, "CreateCol", 1)
+		provider.AssertNumberOfCalls(t, "CreateRow", 1)
+		col.AssertNumberOfCalls(t, "Render", 1)
+		col.AssertNumberOfCalls(t, "SetConfig", 1)
 	})
 }
