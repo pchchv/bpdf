3db118f999b86d1bcb20e2d6eec33fb42d4746d3
pchchv <ipchchv@gmail.com>
2024-11-03 09:01:35 +0300

    Implement a getAbsoluteFilePath method

--- test/config.go
+++ test/config.go
@@ -6,3 +6,7 @@ type Config struct {
 	AbsolutePath string
 	TestPath     string `yaml:"test_path"`
 }
+
+func (c *Config) getAbsoluteFilePath(file string) string {
+	return c.AbsolutePath + c.TestPath + file
+}
