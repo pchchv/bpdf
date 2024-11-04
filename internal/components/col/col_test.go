632a7fbbb31ef308469e5a018325e0ba033694fd
pchchv <ipchchv@gmail.com>
2024-11-04 10:22:14 +0300

    Create test of a New function

--- internal/components/col/col_test.go
+++ internal/components/col/col_test.go
@@ -4,10 +4,12 @@ import (
 	"testing"
 
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/internal/components/code"
 	"github.com/pchchv/bpdf/internal/components/col"
 	"github.com/pchchv/bpdf/internal/fixture"
 	"github.com/pchchv/bpdf/mocks"
 	"github.com/pchchv/bpdf/properties"
+	"github.com/pchchv/bpdf/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -88,5 +90,32 @@ func TestCol_GetHeight(t *testing.T) {
 
 		component.AssertNumberOfCalls(t, "GetHeight", 1)
 		assert.Equal(t, height, 15.0)
+	})
+}
+
+func TestNew(t *testing.T) {
+	t.Run("when size is not defined, should use is as max", func(t *testing.T) {
+		c := col.New()
+
+		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_zero_size.json")
+	})
+
+	t.Run("when size is defined, should not use max", func(t *testing.T) {
+		c := col.New(12)
+
+		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_defined_size.json")
+	})
+
+	t.Run("when has component, should retrieve components", func(t *testing.T) {
+		c := col.New(12).Add(code.NewQr("code"))
+
+		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_with_components.json")
+	})
+
+	t.Run("when has component, should retrieve components", func(t *testing.T) {
+		prop := fixture.CellProp()
+		c := col.New(12).WithStyle(&prop)
+
+		test.New(t).Assert(c.GetStructure()).Equals("components/cols/new_with_properties.json")
 	})
 }
