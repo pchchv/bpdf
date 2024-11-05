6a2839e4a5e119f08fc4468f1a84a9e9c572b6ed
pchchv <ipchchv@gmail.com>
2024-11-05 10:57:21 +0300

    Implement methods GetHeight and GetStructure

--- components/image/fileimage.go
+++ components/image/fileimage.go
@@ -4,6 +4,7 @@ package image
 import (
 	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/node"
 	"github.com/pchchv/bpdf/properties"
 )
 
@@ -21,4 +22,27 @@ func (f *FileImage) Render(provider core.Provider, cell *entity.Cell) {
 // SetConfig sets the pdf config.
 func (f *FileImage) SetConfig(config *entity.Config) {
 	f.config = config
+}
+
+// GetStructure returns the Structure of an Image.
+func (f *FileImage) GetStructure() *node.Node[core.Structure] {
+	str := core.Structure{
+		Type:    "fileImage",
+		Value:   f.path,
+		Details: f.prop.ToMap(),
+	}
+
+	return node.New(str)
+}
+
+// GetHeight returns the height that the image will have in the PDF
+func (f *FileImage) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
+	dimensions, err := provider.GetDimensionsByImage(f.path)
+	if err != nil {
+		return 0.0
+	}
+
+	proportion := dimensions.Height / dimensions.Width
+	width := (f.prop.Percent / 100) * cell.Width
+	return (proportion * width) + f.prop.Top
 }
