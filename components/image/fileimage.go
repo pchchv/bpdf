eef7acb9d8d7c40d5e57e3fcec25f1ba84f27266
pchchv <ipchchv@gmail.com>
2024-11-05 10:56:32 +0300

    Implement methods Render and SetConfig

--- components/image/fileimage.go
+++ components/image/fileimage.go
@@ -2,6 +2,7 @@
 package image
 
 import (
+	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/properties"
 )
@@ -10,4 +11,14 @@ type FileImage struct {
 	path   string
 	prop   properties.Rect
 	config *entity.Config
+}
+
+// Render renders an Image into a PDF context.
+func (f *FileImage) Render(provider core.Provider, cell *entity.Cell) {
+	provider.AddImageFromFile(f.path, cell, &f.prop)
+}
+
+// SetConfig sets the pdf config.
+func (f *FileImage) SetConfig(config *entity.Config) {
+	f.config = config
 }
