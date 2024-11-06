7eced66ef5dc7a901dc548ccdeebd3dd8ccebc44
pchchv <ipchchv@gmail.com>
2024-11-06 10:39:54 +0300

    Create some tests of a New... functions

--- components/image/fileimage_test.go
+++ components/image/fileimage_test.go
@@ -21,3 +21,45 @@ func TestNewFromFile(t *testing.T) {
 		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_custom_prop.json")
 	})
 }
+
+func TestNewFromFileCol(t *testing.T) {
+	t.Run("when prop is not sent, should use default", func(t *testing.T) {
+		sut := image.NewFromFileCol(12, "path")
+
+		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_col_default_prop.json")
+	})
+
+	t.Run("when prop is sent, should use the provided", func(t *testing.T) {
+		sut := image.NewFromFileCol(12, "path", fixture.RectProp())
+
+		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_col_custom_prop.json")
+	})
+}
+
+func TestNewFromFileRow(t *testing.T) {
+	t.Run("when prop is not sent, should use default", func(t *testing.T) {
+		sut := image.NewFromFileRow(10, "path")
+
+		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_row_default_prop.json")
+	})
+
+	t.Run("when prop is sent, should use the provided", func(t *testing.T) {
+		sut := image.NewFromFileRow(12, "path", fixture.RectProp())
+
+		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_row_custom_prop.json")
+	})
+}
+
+func TestNewAutoFromFileRow(t *testing.T) {
+	t.Run("when prop is not sent, should use default", func(t *testing.T) {
+		sut := image.NewAutoFromFileRow("path")
+
+		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_auto_row_default_prop.json")
+	})
+
+	t.Run("when prop is sent, should use the provided", func(t *testing.T) {
+		sut := image.NewAutoFromFileRow("path", fixture.RectProp())
+
+		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_file_auto_row_custom_prop.json")
+	})
+}
