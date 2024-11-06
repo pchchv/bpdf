064b3f31ef34067c3060654da18b7bc42ff7bf46
pchchv <ipchchv@gmail.com>
2024-11-06 10:57:58 +0300

    Implement methods GetStructure and GetHeight

--- components/image/bytesimage.go
+++ components/image/bytesimage.go
@@ -3,7 +3,9 @@ package image
 
 import (
 	"github.com/pchchv/bpdf/consts/extension"
+	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/node"
 	"github.com/pchchv/bpdf/properties"
 )
 
@@ -12,4 +14,35 @@ type BytesImage struct {
 	extension extension.Extension
 	prop      properties.Rect
 	config    *entity.Config
+}
+
+// GetStructure returns the Structure of an Image.
+func (b *BytesImage) GetStructure() *node.Node[core.Structure] {
+	trimLength := 10
+	if len(b.bytes) < trimLength {
+		trimLength = len(b.bytes)
+	}
+
+	str := core.Structure{
+		Type:    "bytesImage",
+		Value:   b.bytes[:trimLength],
+		Details: b.prop.ToMap(),
+	}
+
+	str.Details["extension"] = b.extension
+	str.Details["bytes_size"] = len(b.bytes)
+
+	return node.New(str)
+}
+
+// GetHeight returns the height that the image will have in the PDF
+func (b *BytesImage) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
+	dimensions, err := provider.GetDimensionsByImageByte(b.bytes, b.extension)
+	if err != nil {
+		return 0
+	}
+
+	proportion := dimensions.Height / dimensions.Width
+	width := (b.prop.Percent / 100) * cell.Width
+	return proportion * width
 }
