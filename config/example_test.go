b047fc1ccba0e7cf99eeb74152b109297236c67b
pchchv <ipchchv@gmail.com>
2024-11-06 11:17:41 +0300

    Implement examples of usage of the builder

--- config/example_test.go
+++ config/example_test.go
@@ -3,11 +3,94 @@ package config_test
 import (
 	"github.com/pchchv/bpdf"
 	"github.com/pchchv/bpdf/config"
+	"github.com/pchchv/bpdf/consts/pagesize"
 )
 
 // ExampleNewBuilder demonstrates how to use builder.
 func ExampleNewBuilder() {
 	cfg := config.NewBuilder().Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_Build demonstrates how to build configs.
+func ExampleCfgBuilder_Build() {
+	cfg := config.NewBuilder().Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_WithPageSize demonstrates how to customize page size.
+func ExampleCfgBuilder_WithPageSize() {
+	// If pagesize is invalid, then ignore customization.
+	cfg := config.NewBuilder().
+		WithPageSize(pagesize.A5).
+		Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_WithTopMargin demonstrates how to customize margin.
+func ExampleCfgBuilder_WithTopMargin() {
+	// If top less than minimum, ignore customization.
+	cfg := config.NewBuilder().
+		WithTopMargin(15).
+		Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_WithRightMargin demonstrates how to customize margin.
+func ExampleCfgBuilder_WithRightMargin() {
+	// If top less than minimum, ignore customization.
+	cfg := config.NewBuilder().
+		WithRightMargin(15).
+		Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_WithLeftMargin demonstrates how to customize margin.
+func ExampleCfgBuilder_WithLeftMargin() {
+	// If top less than minimum, ignore customization.
+	cfg := config.NewBuilder().
+		WithLeftMargin(15).
+		Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_WithBottomMargin demonstrates how to customize margin.
+func ExampleCfgBuilder_WithBottomMargin() {
+	// If top less than minimum, ignore customization.
+	cfg := config.NewBuilder().
+		WithBottomMargin(15).
+		Build()
+
+	_ = bpdf.New(cfg)
+
+	// generate document
+}
+
+// ExampleCfgBuilder_WithConcurrentMode demonstrates how to enable concurrent generation.
+func ExampleCfgBuilder_WithConcurrentMode() {
+	// if chunkWorkers is less than 1, then ignore customization.
+	chunkWorkers := 7
+	cfg := config.NewBuilder().
+		WithConcurrentMode(chunkWorkers).
+		Build()
 
 	_ = bpdf.New(cfg)
 
