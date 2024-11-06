9c608a3f6524a3b88ad6424b508034749d750571
pchchv <ipchchv@gmail.com>
2024-11-06 11:02:21 +0300

    Create tests of a method GetHeight

--- components/image/bytesimage_test.go
+++ components/image/bytesimage_test.go
@@ -1,12 +1,16 @@
 package image_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/pchchv/bpdf/components/image"
 	"github.com/pchchv/bpdf/consts/extension"
+	"github.com/pchchv/bpdf/core/entity"
 	"github.com/pchchv/bpdf/internal/fixture"
+	"github.com/pchchv/bpdf/mocks"
 	"github.com/pchchv/bpdf/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewFromBytes(t *testing.T) {
@@ -62,5 +66,29 @@ func TestNewAutoFromBytesRow(t *testing.T) {
 		sut := image.NewAutoFromBytesRow([]byte{1, 2, 3}, extension.Jpg, fixture.RectProp())
 
 		test.New(t).Assert(sut.GetStructure()).Equals("components/images/new_image_from_bytes_auto_row_custom_prop.json")
+	})
+}
+
+func TestBytesImage_GetHeight(t *testing.T) {
+	t.Run("When it is not possible to know the dimensions of the bytes image, should return height 0", func(t *testing.T) {
+		cell := fixture.CellEntity()
+		img := fixture.ImageEntity()
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().GetDimensionsByImageByte(img.Bytes, img.Extension).Return(nil, errors.New("anyError2"))
+		sut := image.NewFromBytes(img.Bytes, img.Extension)
+
+		height := sut.GetHeight(provider, &cell)
+		assert.Equal(t, height, 0.0)
+	})
+
+	t.Run("When the height of the bytes image is half the width, should return half the width of the cell", func(t *testing.T) {
+		cell := fixture.CellEntity()
+		img := fixture.ImageEntity()
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().GetDimensionsByImageByte(img.Bytes, img.Extension).Return(&entity.Dimensions{Width: 10, Height: 5}, nil)
+		sut := image.NewFromBytes(img.Bytes, img.Extension)
+
+		height := sut.GetHeight(provider, &cell)
+		assert.Equal(t, height, cell.Width/2)
 	})
 }
