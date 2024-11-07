4d3f21b9413cef1b85b13b5b4560f8f4db92b132
pchchv <ipchchv@gmail.com>
2024-11-07 11:23:30 +0300

    Create tests of a method GetHeight

--- components/text/text_test.go
+++ components/text/text_test.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/pchchv/bpdf/components/text"
 	"github.com/pchchv/bpdf/internal/fixture"
+	"github.com/pchchv/bpdf/mocks"
+	"github.com/pchchv/bpdf/properties"
 	"github.com/pchchv/bpdf/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNew(t *testing.T) {
@@ -61,5 +64,49 @@ func TestNewAutoRow(t *testing.T) {
 		sut := text.NewAutoRow("code", fixture.TextProp())
 
 		test.New(t).Assert(sut.GetStructure()).Equals("components/texts/new_text_auto_row_custom_prop.json")
+	})
+}
+
+func TestText_GetHeight(t *testing.T) {
+	t.Run("When top margin is sent, should increment row height with top margin", func(t *testing.T) {
+		cell := fixture.CellEntity()
+		font := fixture.FontProp()
+		textProp := properties.Text{Top: 10}
+		textProp.MakeValid(&font)
+		sut := text.New("text", textProp)
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().GetLinesQuantity("text", &textProp, 100.0).Return(5.0)
+		provider.EXPECT().GetFontHeight(&font).Return(2.0)
+
+		height := sut.GetHeight(provider, &cell)
+		assert.Equal(t, 20.0, height)
+	})
+
+	t.Run("When vertical padding is sent, should increment row height with vertical padding", func(t *testing.T) {
+		cell := fixture.CellEntity()
+		font := fixture.FontProp()
+		textProp := properties.Text{VerticalPadding: 5}
+		textProp.MakeValid(&font)
+		sut := text.New("text", textProp)
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().GetLinesQuantity("text", &textProp, 100.0).Return(5.0)
+		provider.EXPECT().GetFontHeight(&font).Return(2.0)
+
+		height := sut.GetHeight(provider, &cell)
+		assert.Equal(t, 30.0, height)
+	})
+
+	t.Run("When font has a height of 2, should return 10", func(t *testing.T) {
+		cell := fixture.CellEntity()
+		font := fixture.FontProp()
+		textProp := properties.Text{}
+		textProp.MakeValid(&font)
+		sut := text.New("text", textProp)
+		provider := mocks.NewProvider(t)
+		provider.EXPECT().GetLinesQuantity("text", &textProp, 100.0).Return(5.0)
+		provider.EXPECT().GetFontHeight(&font).Return(2.0)
+
+		height := sut.GetHeight(provider, &cell)
+		assert.Equal(t, 10.0, height)
 	})
 }
