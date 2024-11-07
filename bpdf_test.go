572a15bc6ce5b01fa2b99d9cab7f21d0f4781a8c
pchchv <ipchchv@gmail.com>
2024-11-07 10:57:15 +0300

    Create tests of AddRow and AddPages methods

--- bpdf_test.go
+++ bpdf_test.go
@@ -5,7 +5,12 @@ import (
 	"testing"
 
 	"github.com/pchchv/bpdf"
+	"github.com/pchchv/bpdf/components/col"
+	"github.com/pchchv/bpdf/components/page"
+	"github.com/pchchv/bpdf/components/row"
 	"github.com/pchchv/bpdf/config"
+	"github.com/pchchv/bpdf/core"
+	"github.com/pchchv/bpdf/test"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,5 +52,89 @@ func TestNew(t *testing.T) {
 
 		assert.NotNil(t, sut)
 		assert.Equal(t, "*bpdf.Bpdf", fmt.Sprintf("%T", sut))
+	})
+}
+
+func TestBPDF_AddRow(t *testing.T) {
+	t.Run("when col is not sent, should empty col is set", func(t *testing.T) {
+		sut := bpdf.New()
+		sut.AddRow(10)
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_row_4.json")
+	})
+
+	t.Run("add one row", func(t *testing.T) {
+		sut := bpdf.New()
+
+		sut.AddRow(10, col.New(12))
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_row_1.json")
+	})
+
+	t.Run("add one row", func(t *testing.T) {
+		sut := bpdf.New()
+
+		sut.AddRow(10, col.New(12))
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_row_1.json")
+	})
+
+	t.Run("add two rows", func(t *testing.T) {
+		sut := bpdf.New()
+
+		sut.AddRow(10, col.New(12))
+		sut.AddRow(10, col.New(12))
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_row_2.json")
+	})
+
+	t.Run("add rows until add new page", func(t *testing.T) {
+		sut := bpdf.New()
+
+		for i := 0; i < 30; i++ {
+			sut.AddRow(10, col.New(12))
+		}
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_row_3.json")
+	})
+}
+
+func TestBPDF_AddPages(t *testing.T) {
+	t.Run("add one page", func(t *testing.T) {
+		sut := bpdf.New()
+
+		sut.AddPages(
+			page.New().Add(
+				row.New(20).Add(col.New(12)),
+			),
+		)
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_pages_1.json")
+	})
+	t.Run("add two pages", func(t *testing.T) {
+		sut := bpdf.New()
+
+		sut.AddPages(
+			page.New().Add(
+				row.New(20).Add(col.New(12)),
+			),
+			page.New().Add(
+				row.New(20).Add(col.New(12)),
+			),
+		)
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_pages_2.json")
+	})
+
+	t.Run("add page greater than one page", func(t *testing.T) {
+		var rows []core.Row
+		sut := bpdf.New()
+		for i := 0; i < 15; i++ {
+			rows = append(rows, row.New(20).Add(col.New(12)))
+		}
+
+		sut.AddPages(page.New().Add(rows...))
+
+		test.New(t).Assert(sut.GetStructure()).Equals("bpdf_add_pages_3.json")
 	})
 }
