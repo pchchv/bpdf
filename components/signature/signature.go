85a347e13a0d217e51fe17a399bc2dfdaa928c9d
pchchv <ipchchv@gmail.com>
2024-11-07 11:27:13 +0300

    Implement GetHeight and GetStructure methods

--- components/signature/signature.go
+++ components/signature/signature.go
@@ -2,7 +2,9 @@
 package signature
 
 import (
+	"github.com/pchchv/bpdf/core"
 	"github.com/pchchv/bpdf/core/entity"
+	"github.com/pchchv/bpdf/node"
 	"github.com/pchchv/bpdf/properties"
 )
 
@@ -10,4 +12,20 @@ type Signature struct {
 	value  string
 	prop   properties.Signature
 	config *entity.Config
+}
+
+// GetStructure returns the Structure of a Signature.
+func (s *Signature) GetStructure() *node.Node[core.Structure] {
+	str := core.Structure{
+		Type:    "signature",
+		Value:   s.value,
+		Details: s.prop.ToMap(),
+	}
+
+	return node.New(str)
+}
+
+// GetHeight returns the height that the signature will have in the PDF.
+func (s *Signature) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
+	return s.prop.LineThickness + provider.GetFontHeight(s.prop.ToFontProp())*s.prop.SafePadding
 }
