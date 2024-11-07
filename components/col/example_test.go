1a10d78b6991ad361f0bfb75a75a14b0c17f0924
pchchv <ipchchv@gmail.com>
2024-11-07 11:33:17 +0300

    Implement an example of adding components to Col

--- components/col/example_test.go
+++ components/col/example_test.go
@@ -2,8 +2,11 @@ package col_test
 
 import (
 	"github.com/pchchv/bpdf"
+	"github.com/pchchv/bpdf/components/code"
 	"github.com/pchchv/bpdf/components/col"
 	"github.com/pchchv/bpdf/components/row"
+	"github.com/pchchv/bpdf/components/signature"
+	"github.com/pchchv/bpdf/components/text"
 	"github.com/pchchv/bpdf/consts/border"
 	"github.com/pchchv/bpdf/consts/linestyle"
 	"github.com/pchchv/bpdf/properties"
@@ -15,6 +18,25 @@ func ExampleNew() {
 	// will apply the maximum size, even if custom size is applied.
 	size := 12
 	col := col.New(size)
+
+	row := row.New(10).Add(col)
+
+	m := bpdf.New()
+	m.AddRows(row)
+
+	// Do things and generate
+	_, _ = m.Generate()
+}
+
+// ExampleCol_Add demonstrates how to add components to Col.
+func ExampleCol_Add() {
+	col := col.New()
+
+	text := text.New("text content")
+	qrCode := code.NewQr("qrcode")
+	signature := signature.New("signature label")
+
+	col.Add(text, qrCode, signature)
 
 	row := row.New(10).Add(col)
 
