32233f0f9771402c2aeb35361bae213bdfadbc10
pchchv <ipchchv@gmail.com>
2024-11-07 11:28:13 +0300

    Implement SetConfig and Render methods

--- components/signature/signature.go
+++ components/signature/signature.go
@@ -29,3 +29,18 @@ func (s *Signature) GetStructure() *node.Node[core.Structure] {
 func (s *Signature) GetHeight(provider core.Provider, cell *entity.Cell) float64 {
 	return s.prop.LineThickness + provider.GetFontHeight(s.prop.ToFontProp())*s.prop.SafePadding
 }
+
+// Render renders a Signature into a PDF context.
+func (s *Signature) Render(provider core.Provider, cell *entity.Cell) {
+	fontSize := provider.GetFontHeight(s.prop.ToFontProp()) * s.prop.SafePadding
+	textProp := s.prop.ToTextProp(align.Center, cell.Height-fontSize, 0)
+	offsetPercent := (cell.Height - fontSize) / cell.Height * 100.0
+
+	provider.AddText(s.value, cell, textProp)
+	provider.AddLine(cell, s.prop.ToLineProp(offsetPercent))
+}
+
+// SetConfig sets the config.
+func (s *Signature) SetConfig(config *entity.Config) {
+	s.config = config
+}
