8929649821e6d121865b2ce10ebe1df78970c741
pchchv <ipchchv@gmail.com>
2024-11-08 12:18:50 +0300

    Implement methods getAVG and buildMetrics

--- metricsdecorator.go
+++ metricsdecorator.go
@@ -16,3 +16,85 @@ type MetricsDecorator struct {
 	structureTime  *metrics.Time
 	inner          core.BPDF
 }
+
+func (m *MetricsDecorator) getAVG(times []*metrics.Time) *metrics.Time {
+	var sum float64
+	for _, time := range times {
+		sum += time.Value
+	}
+
+	return &metrics.Time{
+		Value: sum / float64(len(times)),
+		Scale: times[0].Scale,
+	}
+}
+
+func (m *MetricsDecorator) buildMetrics(bytesSize int) *metrics.Report {
+	var timeMetrics []metrics.TimeMetric
+	if m.structureTime != nil {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "get_tree_structure",
+			Times: []*metrics.Time{m.structureTime},
+			Avg:   m.structureTime,
+		})
+	}
+
+	if m.generateTime != nil {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "generate",
+			Times: []*metrics.Time{m.generateTime},
+			Avg:   m.generateTime,
+		})
+	}
+
+	if m.headerTime != nil {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "header",
+			Times: []*metrics.Time{m.headerTime},
+			Avg:   m.headerTime,
+		})
+	}
+
+	if m.footerTime != nil {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "footer",
+			Times: []*metrics.Time{m.footerTime},
+			Avg:   m.footerTime,
+		})
+	}
+
+	if len(m.addPageTime) > 0 {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "add_page",
+			Times: m.addPageTime,
+			Avg:   m.getAVG(m.addPageTime),
+		})
+	}
+
+	if len(m.addRowTime) > 0 {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "add_row",
+			Times: m.addRowTime,
+			Avg:   m.getAVG(m.addRowTime),
+		})
+	}
+
+	if len(m.addRowsTime) > 0 {
+		timeMetrics = append(timeMetrics, metrics.TimeMetric{
+			Key:   "add_rows",
+			Times: m.addRowsTime,
+			Avg:   m.getAVG(m.addRowsTime),
+		})
+	}
+
+	return &metrics.Report{
+		TimeMetrics: timeMetrics,
+		SizeMetric: metrics.SizeMetric{
+			Key: "file_size",
+			Size: metrics.Size{
+				Value: float64(bytesSize),
+				Scale: metrics.Byte,
+			},
+		},
+	}
+}
