4a90c13bad193ca433532d5469b693d994da962e
pchchv <ipchchv@gmail.com>
2024-11-09 05:58:54 +0300

    Implement the examples of creating a metrics decorator and adding a new page

--- example_test.go
+++ example_test.go
@@ -2,6 +2,8 @@ package bpdf_test
 
 import (
 	"github.com/pchchv/bpdf"
+	"github.com/pchchv/bpdf/components/code"
+	"github.com/pchchv/bpdf/components/page"
 	"github.com/pchchv/bpdf/config"
 )
 
@@ -15,4 +17,29 @@ func ExampleNew() {
 
 	// Do things and generate
 	_, _ = m.Generate()
+}
+
+// ExampleNewMetricsDecorator demonstrates how to create a bpdf metrics decorator instance.
+func ExampleNewMetricsDecorator() {
+	// optional
+	b := config.NewBuilder()
+	cfg := b.Build()
+
+	mrt := bpdf.New(cfg)               // cfg is an optional
+	m := bpdf.NewMetricsDecorator(mrt) // decorator of bpdf
+
+	// Do things and generate
+	_, _ = m.Generate()
+}
+
+// ExampleBPDF_AddPages demonstrates how to add a new page in bpdf.
+func ExampleBPDF_AddPages() {
+	m := bpdf.New()
+
+	p := page.New()
+	p.Add(code.NewBarRow(10, "barcode"))
+
+	m.AddPages(p)
+
+	// Do things and generate
 }
