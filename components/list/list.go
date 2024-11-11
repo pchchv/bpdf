dd6412c044159c957f06f43ddfb07a95c6b7cb5a
pchchv <ipchchv@gmail.com>
2024-11-11 17:25:54 +0300

    Implement a method Build

--- components/list/list.go
+++ components/list/list.go
@@ -1,7 +1,11 @@
 // Package list implements creation of lists (old tablelist).
 package list
 
-import "github.com/pchchv/bpdf/core"
+import (
+	"errors"
+
+	"github.com/pchchv/bpdf/core"
+)
 
 // Listable is the main abstraction of a listable item in a TableList.
 // A collection of objects that implements this
@@ -9,4 +13,20 @@ import "github.com/pchchv/bpdf/core"
 type Listable interface {
 	GetHeader() core.Row
 	GetContent(i int) core.Row
+}
+
+// Build is responsible to receive a collection of objects that
+// implements Listable and build the rows of TableList.
+// This method should be used in case of a collection of values.
+func Build[T Listable](arr []T) (rows []core.Row, err error) {
+	if len(arr) == 0 {
+		return nil, errors.New("empty array")
+	}
+
+	rows = append(rows, arr[0].GetHeader())
+	for i, element := range arr {
+		rows = append(rows, element.GetContent(i))
+	}
+
+	return rows, nil
 }
