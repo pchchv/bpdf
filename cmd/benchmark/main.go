7f719e668de5f270443c320f3541f66c1ce7a27d
pchchv <ipchchv@gmail.com>
2024-11-11 17:09:04 +0300

    Implement buildTextsRow, buildHeader and buildFooter funcs

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -5,8 +5,10 @@ import (
 	"os"
 
 	"github.com/pchchv/bpdf/components/code"
+	"github.com/pchchv/bpdf/components/col"
 	"github.com/pchchv/bpdf/components/image"
 	"github.com/pchchv/bpdf/components/row"
+	"github.com/pchchv/bpdf/components/signature"
 	"github.com/pchchv/bpdf/components/text"
 	"github.com/pchchv/bpdf/consts/align"
 	"github.com/pchchv/bpdf/consts/extension"
@@ -46,6 +48,48 @@ func buildImagesRow() []core.Row {
 		row.New(20).Add(
 			text.NewCol(4, "Image From Bytes:", properties.Text{Size: 15, Top: 6, Align: align.Center}),
 			image.NewFromBytesCol(8, bytes, extension.Png, properties.Rect{Center: true, Percent: 90}),
+		),
+	}
+}
+
+func buildTextsRow() []core.Row {
+	colText := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac condimentum sem."
+
+	return []core.Row{
+		row.New(20).Add(
+			text.NewCol(4, "Text:", properties.Text{Size: 15, Top: 6, Align: align.Center}),
+			text.NewCol(8, colText, properties.Text{Size: 12, Top: 5, Align: align.Center}),
+		),
+		row.New(40).Add(
+			text.NewCol(4, "Signature:", properties.Text{Size: 15, Top: 17, Align: align.Center}),
+			signature.NewCol(8, "Name", properties.Signature{FontSize: 10}),
+		),
+	}
+}
+
+func buildHeader() []core.Row {
+	r1 := row.New(30).Add(
+		col.New(12).Add(
+			text.New("Config V2", properties.Text{
+				Top:   5,
+				Size:  15,
+				Align: align.Center,
+			}),
+			text.New("Grid system, fast generation, embedded metrics and testable.", properties.Text{
+				Top:   13,
+				Size:  13,
+				Align: align.Center,
+			}),
+		),
+	)
+
+	return []core.Row{r1}
+}
+
+func buildFooter() []core.Row {
+	return []core.Row{
+		row.New(10).Add(
+			text.NewCol(2, "GitHub: https://github.com/pchchv/bpdf/"),
 		),
 	}
 }
