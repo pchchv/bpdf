6d13b2bc65c3d5b19ebb40a4edd0f518a5fe66cd
pchchv <ipchchv@gmail.com>
2024-11-12 13:57:06 +0300

    Implement a seriesanalyzer

--- cmd/seriesanalyzer/main.go
+++ cmd/seriesanalyzer/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func mergeArrays(a []float64, b []float64) (merged []float64) {
 	var i, j int
 	for i < len(a) && j < len(b) {
@@ -35,4 +43,30 @@ func MergeFloat64(arr []float64) []float64 {
 }
 
 func main() {
+	bytes, err := os.ReadFile("docs/assets/text/benchmark.txt")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stringContent := string(bytes)
+
+	var sum float64
+	var values []float64
+	lines := strings.Split(stringContent, "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		value, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		values = append(values, value)
+		sum += value
+	}
+
+	values = MergeFloat64(values)
+
+	fmt.Printf("min: %f, max: %f, avg: %f", values[0], values[len(values)-1], sum/float64(len(values)))
 }
